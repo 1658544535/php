2f1a740bd206c58f5a687f827b6c29d3d46d832d
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-23 02:00:03 +0000

    首页活动推荐修改 把gid更改为activity_id

--- ajaxtpl/ajax_index.php
+++ ajaxtpl/ajax_index.php
@@ -7,6 +7,7 @@ $FocusSettingModel         = M('focus_setting');
 $GrouponActivityModel      = M('groupon_activity');
 $GrouponUserRecordModel    = M('groupon_user_record');
 $GrouponFreeCouponModel    = M('groupon_free_coupon');
+$GrouponRecommendModel     = M('groupon_recommend');
 $typeId = CheckDatas( 'tid', '' );
 
 
@@ -17,12 +18,12 @@ $objBannerImages  = $FocusSettingModel->getAll(array('status'=>1),'','',3);
 	
 //获取拼团活动数据
 	
-$ObjGrouponList = $GrouponActivityModel->query("SELECT g.id as gid, g.price,  g.status, g.activity_status, g.type, g.banner, g.num, p.id, p.product_name, p.image , p.selling_price, p.sell_number, p.product_sketch, p.image_main FROM `groupon_activity` AS g LEFT JOIN product AS p on p.`id` = g.`product_id` WHERE 1=1 AND g.status =1 AND g.type =1 AND g.activity_status !=0 ORDER BY g.sorting DESC,g.create_date DESC ",false,true,$page);
+$ObjGrouponList = $GrouponRecommendModel->query("SELECT gr.activity_id,  g.price,  g.status, g.activity_status, g.type, g.banner, g.num, p.id, p.product_name, p.image , p.selling_price, p.sell_number, p.product_sketch, p.image_main FROM `groupon_recommend` AS gr LEFT JOIN `groupon_activity` AS g on g.`id` = gr.`activity_id` LEFT JOIN product AS p on p.`id` = g.`product_id` WHERE 1=1 AND g.status =1 AND g.type =1 AND g.activity_status !=0 ORDER BY gr.sorting DESC,gr.create_date DESC ",false,true,$page);
 
 	
 //获取分类拼团活动数据
 	
-$ObjProductList = $GrouponActivityModel->query("SELECT g.id as gid, g.price,  g.status, g.activity_status, g.type, g.banner, g.num, p.id, p.product_name, p.image , p.selling_price, p.sell_number, p.product_sketch, p.image_main, p.image_small FROM `groupon_activity` AS g LEFT JOIN product AS p on p.`id` = g.`product_id` WHERE 1=1 AND g.status =1 AND g.type =1 AND g.activity_status !=0 AND p.product_type_id ='".$typeId."' ORDER BY g.sorting DESC,g.create_date DESC ",false,true,$page,20);
+$ObjProductList = $GrouponActivityModel->query("SELECT g.id as gid, g.price,  g.status, g.activity_status, g.type, g.banner, g.num, p.id, p.product_name, p.image , p.selling_price, p.sell_number, p.product_sketch, p.image_main, p.image_small FROM `groupon_activity` AS g LEFT JOIN product AS p on p.`id` = g.`product_id` WHERE 1=1 AND g.status =1 AND g.type =1 AND g.activity_status !=0 AND g.is_default ==1 AND p.product_type_id ='".$typeId."' ORDER BY g.sorting DESC,g.create_date DESC ",false,true,$page,20);
 
 
 	$Data = array(
