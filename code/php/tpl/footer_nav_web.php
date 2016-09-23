f7ecc8c73a37291bee6fd366dbbdede9974b4411
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-23 06:12:00 +0000

    增加底部导航模板文件

---
+++ code/php/tpl/footer_nav_web.php
@@ -0,0 +1,18 @@
+<nav class="bar bar-tab">
+	<a class="tab-item active" href="index.html">
+		<span class="icon i-home"></span>
+		<span class="tab-label">首页</span>
+	</a>
+	<a class="tab-item" href="demo2.html">
+		<span class="icon i-price"></span>
+		<span class="tab-label">猜价格</span>
+	</a>
+	<a class="tab-item" href="#">
+		<span class="icon i-search"></span>
+		<span class="tab-label">搜索</span>
+	</a>
+	<a class="tab-item" href="#">
+		<span class="icon i-user"></span>
+		<span class="tab-label">个人中心</span>
+	</a>
+</nav>
