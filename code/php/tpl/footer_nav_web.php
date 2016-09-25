637527a8045cc00ffd0de023347152768c3c7324
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-25 01:34:19 +0000

    调整底部导航

--- code/php/tpl/footer_nav_web.php
+++ code/php/tpl/footer_nav_web.php
@@ -1,17 +1,17 @@
 <nav class="bar bar-tab">
-	<a class="tab-item <?php echo $footerNavActive['index'];?>" href="/">
+	<a class="tab-item <?php echo ($footerNavActive=='index')?'active':'';?>" href="/">
 		<span class="icon i-home"></span>
 		<span class="tab-label">首页</span>
 	</a>
-	<a class="tab-item <?php echo $footerNavActive['guess'];?>" href="product_guess_price.php">
+	<a class="tab-item <?php echo ($footerNavActive=='guess')?'active':'';?>" href="product_guess_price.php">
 		<span class="icon i-price"></span>
 		<span class="tab-label">猜价格</span>
 	</a>
-	<a class="tab-item <?php echo $footerNavActive['search'];?>" href="#">
+	<a class="tab-item <?php echo ($footerNavActive=='search')?'active':'';?>" href="#">
 		<span class="icon i-search"></span>
 		<span class="tab-label">搜索</span>
 	</a>
-	<a class="tab-item <?php echo $footerNavActive['user'];?>" href="user.php">
+	<a class="tab-item <?php echo ($footerNavActive=='user')?'active':'';?>" href="user.php">
 		<span class="icon i-user"></span>
 		<span class="tab-label">个人中心</span>
 	</a>
