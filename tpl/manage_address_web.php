b2f94b98df3f056108cc927e43bf32355dd087f6
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-27 04:02:04 +0000

    地址管理

--- tpl/manage_address_web.php
+++ tpl/manage_address_web.php
@@ -165,6 +165,7 @@
                 <a href="javascript:;">添加新地址</a>
             </div>
 
+<!--
             <script id='tpl_pull' type="text/template">
                 <%for(var i=0,len=data["data"].length;i<len; i++){%>
                     <li data-id="<%=data["data"][i].addId%>">
@@ -183,6 +184,27 @@
                     </li>
                 <%}%>
             </script>
+			-->
+
+			<script id='tpl_pull' type="text/template">
+                <%for(var i=0;i<data["data"].length; i++){%>
+                    <li data-id="<%=data["data"][i].addId%>">
+                        <div class="txt">
+                            <div class="info">
+                                <span class="phone a-t-1"><%=data["data"][i].tel%></span>
+                                <span class="a-t-2"><%=data["data"][i].name%></span>
+                            </div>
+                            <div class="address a-t-4"><%=data["data"][i].address%></div>
+                        </div>
+                        <div class="option">
+                            <a href="javascript:;" data-id="<%=data["data"][i].addId%>" class="default"><i></i>设为默认</a>
+                            <a href="javascript:;" data-id="<%=data["data"][i].addId%>" class="edit"><i></i>编辑</a>
+                            <a href="javascript:;" data-id="<%=data["data"][i].addId%>" class="del"><i></i>删除</a>
+                        </div>
+                    </li>
+                <%}%>
+            </script>
+
 
 			<script type="text/javascript">
 			$(document).on("pageInit", "#page-address", function(e, pageId, page) {
@@ -190,6 +212,9 @@
 
 		    	$(document).off("click", "ul.list-container li a.default").on("click", "ul.list-container li a.default", function(){
 					var _id = $(this).attr("data-id");
+					$.post(_apiUrl+"address_default", {"id":_id}, function(r){
+						location.reload();
+					}, "json");
 				});
 
 		    	$(document).off("click", "ul.list-container li a.del").on("click", "ul.list-container li a.del", function(){
@@ -204,9 +229,68 @@
 						}, "json");
 					});
 				});
+
+				$(document).on("click", ".user-address .edit", function(){
+					$(".p-a-1").val($(".a-t-1").html());
+					$(".p-a-2").val($(".a-t-2").html());
+					$(".p-a-3").val($(".a-t-3").html());
+					$(".p-a-4").val($(".a-t-4").html());
+					$("input[name='id']").val($(this).attr("data-id"));
+					$.popup('.popup-address');
+				});
+
+				$(document).on("click", ".user-address-add a", function(){
+					$(".popup-address input").not('input[type="submit"]').val('');
+					$(".popup-address textarea").val('');
+					$("input[name='id']").val(0);
+					$.popup('.popup-address');
+				});
+
+				$(document).on("submit", ".popup-address form", function(e){
+					var _this = $(this);
+					e.preventDefault();
+					$.ajax({
+						url: _apiUrl+'address_edit',
+						data: _this.serialize(),
+						dataType: 'json',
+						success: function(){
+							location.reload();
+						},
+						failure: function(){
+							$.toast("操作失败");
+						}
+					});
+				});
 		    });
 			</script>
+        </div>
 
+		<script type="text/javascript" src="js/sui/sm-city-picker.min.js" charset="utf-8"></script>
+        <div class="popup popup-address">
+            <div>
+                <a href="#" class="close-popup"></a>
+                <form action="" method="" accept-charset="utf-8">
+					<input type="hidden" name="id" />
+                    <ul>
+                        <li>
+                            <span class="label">收货人:</span>
+                            <div class="main"><input type="text" name="name" class="txt p-a-2" placeholder="请输入收货人" /></div>
+                        </li>
+                        <li>
+                            <span class="label">联系方式:</span>
+                            <div class="main"><input type="text" name="phone" class="txt p-a-1" placeholder="请输入联系方式" /></div>
+                        </li>
+                        <li>
+                            <span class="label">收货地址:</span>
+                            <div class="main">
+                                <input id="city-picker" type="text" class="txt p-a-3" placeholder="请选择省市区" value="" />
+                                <textarea rows="2" name="addr" class="txt p-a-4" placeholder="请输入详细地址"></textarea>
+                            </div>
+                        </li>
+                    </ul>
+                    <input type="submit" class="go" value="提交">
+                </form>
+            </div>
         </div>
     </div>
 </body>
--- user.php
+++ user.php
@@ -2,49 +2,54 @@
 define('HN1', true);
 require_once('./global.php');
 
-/*----------------------------------------------------------------------------------------------------
-	-- 判断是否登录
------------------------------------------------------------------------------------------------------*/
-//$bLogin = $_SESSION['is_login'];
+///*----------------------------------------------------------------------------------------------------
+//	-- 判断是否登录
+//-----------------------------------------------------------------------------------------------------*/
+////$bLogin = $_SESSION['is_login'];
+//
+///*----------------------------------------------------------------------------------------------------
+//	-- 配置
+//-----------------------------------------------------------------------------------------------------*/
+//$return_url   		        = CheckDatas( 'return_url', '/index' );
+//
+//$UserInfoModel 				= M('user_info');
+//$UserCollectModel 			= D('UserCollect');
+//$UserSpecialCollectModel 	= D('UserSpecialCollect');
+//$HistoryModel 				= D('History');
+//$UserOrderModel 			= D('UserOrder');
+//
+///*----------------------------------------------------------------------------------------------------
+//	-- 基本资料
+//-----------------------------------------------------------------------------------------------------*/
+//
+//$objUserInfo   				= $UserInfoModel->get( array('user_id'=>$userid) );
+//
+//// 商品收藏数量
+//$count_product = $UserCollectModel->getCollectNum( $userid );
+//
+//// 专场收藏数量
+//$count_special = $UserSpecialCollectModel->getCollectNum( $userid );
+//
+//// 足迹浏览数量
+//$count_history = $HistoryModel->getHistoryCount( $userid );
+//
+//// 订单数
+//$onway_orders1 	  = $UserOrderModel->getOrderStatus( $userid, 1 );	 				// 待付款
+//$onway_orders2 	  = $UserOrderModel->getOrderStatus( $userid, 2 );	 				// 待发货订单
+//$onway_orders3 	  = $UserOrderModel->getOrderStatus( $userid, 3 );	 				// 待收货订单
+//$onway_orders4 	  = $UserOrderModel->getOrderStatus( $userid, 4 );	 				// 待评价订单
+//
+//// 售后
+//$UserOrderRefundModel = D('UserOrderRefund');
+//$order_refund_num 	  = $UserOrderRefundModel->getUserOrderRefundCount( $userid );	 // 我的足迹
+//
+//$objLogin = M('sys_login');
+//$user = $objLogin->get(array('id'=>$userid));
 
-/*----------------------------------------------------------------------------------------------------
-	-- 配置
------------------------------------------------------------------------------------------------------*/
-$return_url   		        = CheckDatas( 'return_url', '/index' );
-
-$UserInfoModel 				= M('user_info');
-$UserCollectModel 			= D('UserCollect');
-$UserSpecialCollectModel 	= D('UserSpecialCollect');
-$HistoryModel 				= D('History');
-$UserOrderModel 			= D('UserOrder');
-
-/*----------------------------------------------------------------------------------------------------
-	-- 基本资料
------------------------------------------------------------------------------------------------------*/
-
-$objUserInfo   				= $UserInfoModel->get( array('user_id'=>$userid) );
-
-// 商品收藏数量
-$count_product = $UserCollectModel->getCollectNum( $userid );
-
-// 专场收藏数量
-$count_special = $UserSpecialCollectModel->getCollectNum( $userid );
-
-// 足迹浏览数量
-$count_history = $HistoryModel->getHistoryCount( $userid );
-
-// 订单数
-$onway_orders1 	  = $UserOrderModel->getOrderStatus( $userid, 1 );	 				// 待付款
-$onway_orders2 	  = $UserOrderModel->getOrderStatus( $userid, 2 );	 				// 待发货订单
-$onway_orders3 	  = $UserOrderModel->getOrderStatus( $userid, 3 );	 				// 待收货订单
-$onway_orders4 	  = $UserOrderModel->getOrderStatus( $userid, 4 );	 				// 待评价订单
-
-// 售后
-$UserOrderRefundModel = D('UserOrderRefund');
-$order_refund_num 	  = $UserOrderRefundModel->getUserOrderRefundCount( $userid );	 // 我的足迹
-
-$objLogin = M('sys_login');
-$user = $objLogin->get(array('id'=>$userid));
+$info = apiData('myInfoApi.do', array('userId'=>$userid));
+$info = $info['result'];
+!empty($info['couponBTime']) && $info['couponBTime'] = strtotime($info['couponBTime']);
+!empty($info['couponETime']) && $info['couponETime'] = strtotime($info['couponETime']);
 
 $footerNavActive = 'user';
 
