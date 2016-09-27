51bc447c953e9bb53334949d1397805ffb83f8ab
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-27 13:20:54 +0000

    拼团详情、参团详情改为调用接口

--- code/php/groupon.php
+++ code/php/groupon.php
@@ -4,42 +4,52 @@ require_once('./global.php');
 
 $backUrl = getPrevUrl();
 
-$activeId = intval($_GET['id']);
-empty($activeId) && redirect($backUrl);
+$grouponId = intval($_GET['id']);
+empty($grouponId) && redirect($backUrl);
 
-//拼团信息
-$objGroupon = M('groupon_activity');
-$groupon = $objGroupon->get(array('id'=>$activeId,'status'=>1,'is_delete'=>0), '*', ARRAY_A);
-empty($groupon) && redirect($backUrl, '没有相关的活动');
-($groupon['type'] == 3) && redirect($backUrl);//猜价则跳出
+////拼团信息
+//$objGroupon = M('groupon_activity');
+//$groupon = $objGroupon->get(array('id'=>$activeId,'status'=>1,'is_delete'=>0), '*', ARRAY_A);
+//empty($groupon) && redirect($backUrl, '没有相关的活动');
+//($groupon['type'] == 3) && redirect($backUrl);//猜价则跳出
+//
+////团免券(是否可团免)
+//$objGCpn = D('GrouponFreeCoupon');
+//$grouponCpn = $objGCpn->getUserCoupon($userid, array('valid'=>true,'intime'=>true));
+//$isLeader = empty($grouponCpn) ? false : true;
+//(($groupon['type']==2) && !$isLeader) && redirect($backUrl, '没有资格参与免团');
+//
+////商品信息
+//$objPro = D('Product');
+//$product = $objPro->getInfo($groupon['product_id']);
+//
+////其他团购
+//$time = time();
+//$date = date('Y-m-d H:i:s', $time);
+//$sql = "SELECT ga.`id`,ga.`product_id`,ga.`end_time`,ga.`num`,IFNULL(tmp.`buyernum`,0) AS buyernum,(ga.`num`-IFNULL(tmp.`buyernum`,0)) AS remain FROM `groupon_activity` AS ga LEFT JOIN (SELECT `activity_id`,`user_id`,COUNT(*) AS buyernum FROM `groupon_user_record` GROUP BY `activity_id`) AS tmp ON ga.`id`=tmp.`activity_id` LEFT JOIN `sys_login` AS sl ON tmp.`user_id`=sl.`id` WHERE ga.`status`=1 AND ga.`type`=1 AND ga.`is_delete`=0 AND ga.`begin_time`<='{$date}' AND ga.`end_time`>'{$date}' AND ga.`id`<>{$groupon['id']} ORDER BY remain ASC,ga.`end_time` ASC LIMIT 3";
+//$otherActives = $objGroupon->query($sql);
+//$otherActIds = array();
+//foreach($otherActives as $k => $v){
+//	$otherActIds[] = $v->id;
+//	$v->remainSec = strtotime($v->end_time)-$time;
+//}
+//$leaders = array();
+//if(!empty($otherActIds)){
+//	$sql = 'SELECT gur.`activity_id`,gur.`user_id`,sl.`image`,sl.`name` FROM `sys_login` AS sl LEFT JOIN `groupon_user_record` AS gur ON sl.`id`=gur.`user_id` WHERE gur.`activity_id` IN ('.implode(',', $otherActIds).') AND gur.`is_head`=1';
+//	$rs = $objGroupon->query($sql);
+//	foreach($rs as $k => $v){
+//		$v->image = getFullAvatar($v->image);
+//		$leaders[$v->activity_id] = $v;
+//	}
+//}
 
-//团免券(是否可团免)
-$objGCpn = D('GrouponFreeCoupon');
-$grouponCpn = $objGCpn->getUserCoupon($userid, array('valid'=>true,'intime'=>true));
-$isLeader = empty($grouponCpn) ? false : true;
-(($groupon['type']==2) && !$isLeader) && redirect($backUrl, '没有资格参与免团');
+$info = apiData('openGroupActivityApi.do', array('activityId'=>$grouponId, 'userId'=>$userid));
+!$info['success'] && redirect($backUrl, $info['error_msg']);
 
-//商品信息
-$objPro = D('Product');
-$product = $objPro->getInfo($groupon['product_id']);
-
-//其他团购
-$time = time();
-$date = date('Y-m-d H:i:s', $time);
-$sql = "SELECT ga.`id`,ga.`product_id`,ga.`end_time`,ga.`num`,IFNULL(tmp.`buyernum`,0) AS buyernum,(ga.`num`-IFNULL(tmp.`buyernum`,0)) AS remain FROM `groupon_activity` AS ga LEFT JOIN (SELECT `activity_id`,`user_id`,COUNT(*) AS buyernum FROM `groupon_user_record` GROUP BY `activity_id`) AS tmp ON ga.`id`=tmp.`activity_id` LEFT JOIN `sys_login` AS sl ON tmp.`user_id`=sl.`id` WHERE ga.`status`=1 AND ga.`type`=1 AND ga.`is_delete`=0 AND ga.`begin_time`<='{$date}' AND ga.`end_time`>'{$date}' AND ga.`id`<>{$groupon['id']} ORDER BY remain ASC,ga.`end_time` ASC LIMIT 3";
-$otherActives = $objGroupon->query($sql);
-$otherActIds = array();
-foreach($otherActives as $k => $v){
-	$otherActIds[] = $v->id;
-	$v->remainSec = strtotime($v->end_time)-$time;
-}
-$leaders = array();
-if(!empty($otherActIds)){
-	$sql = 'SELECT gur.`activity_id`,gur.`user_id`,sl.`image`,sl.`name` FROM `sys_login` AS sl LEFT JOIN `groupon_user_record` AS gur ON sl.`id`=gur.`user_id` WHERE gur.`activity_id` IN ('.implode(',', $otherActIds).') AND gur.`is_head`=1';
-	$rs = $objGroupon->query($sql);
-	foreach($rs as $k => $v){
-		$v->image = getFullAvatar($v->image);
-		$leaders[$v->activity_id] = $v;
+$info = $info['result'];
+if(!empty($info['waitGroupList'])){
+	foreach($info['waitGroupList'] as $k => $v){
+		$v->remainSec = $v->endTime - $v->nowTime;
 	}
 }
 
--- code/php/groupon_join.php
+++ code/php/groupon_join.php
@@ -4,45 +4,52 @@ require_once('./global.php');
 
 $backUrl = getPrevUrl();
 
-$grouponId = CheckDatas('id', 0);
+$grouponId = intval($_GET['id']);
 empty($grouponId) && redirect($backUrl, '参数错误');
 
-$time = time();
+//$time = time();
 
-//拼团信息
-$objGroupon = M('groupon_activity');
-$groupon = $objGroupon->get(array('id'=>$grouponId,'status'=>1,'is_delete'=>0), '*', ARRAY_A);
-empty($groupon) && redirect($backUrl, '没有相关的活动');
-($groupon['type'] == 3) && redirect($backUrl);//猜价则跳出
-$groupon['remainSec'] = strtotime($groupon['end_time'])-$time;
+////拼团信息
+//$objGroupon = M('groupon_activity');
+//$groupon = $objGroupon->get(array('id'=>$grouponId,'status'=>1,'is_delete'=>0), '*', ARRAY_A);
+//empty($groupon) && redirect($backUrl, '没有相关的活动');
+//($groupon['type'] == 3) && redirect($backUrl);//猜价则跳出
+//$groupon['remainSec'] = strtotime($groupon['end_time'])-$time;
+//
+//$objUserRs = M('groupon_user_record');
+//
+//$sql = "SELECT gur.`user_id`,gur.`attend_time`,gur.`is_head`,sl.`name` AS uname,sl.`image` FROM `groupon_user_record` AS gur LEFT JOIN `sys_login` AS sl ON gur.`user_id`=sl.`id` WHERE gur.`activity_id`={$grouponId} ORDER BY gur.`is_head` DESC,gur.`attend_time` ASC,gur.`id` ASC";
+//$rs = $objUserRs->query($sql);
+////empty($rs) && redirect('groupon.php?id='.$grouponId, '未开团，现在开团');
+//
+//$joiners = array();
+//foreach($rs as $v){
+//	$v->image = getFullAvatar($v->image);
+//	$joiners[] = $v;
+//}
+//
+//$joinerCount = count($joiners);
+//
+//if(strtotime($groupon['end_time']) < $time){//已结束
+//
+//}else{//进行中
+//	if($joinerCount < $groupon['num']){//人数未满
+//		
+//	}
+//}
+//
+////商品信息
+//$objPro = D('Product');
+//$product = $objPro->getInfo($groupon['product_id']);
 
-$objUserRs = M('groupon_user_record');
+$info = apiData('groupDetailApi.do', array('activityId'=>$grouponId, 'userId'=>$userid));
+!$info['success'] && redirect($backUrl, $info['error_msg']);
 
-$sql = "SELECT gur.`user_id`,gur.`attend_time`,gur.`is_head`,sl.`name` AS uname,sl.`image` FROM `groupon_user_record` AS gur LEFT JOIN `sys_login` AS sl ON gur.`user_id`=sl.`id` WHERE gur.`activity_id`={$grouponId} ORDER BY gur.`is_head` DESC,gur.`attend_time` ASC,gur.`id` ASC";
-$rs = $objUserRs->query($sql);
-//empty($rs) && redirect('groupon.php?id='.$grouponId, '未开团，现在开团');
-
-$joiners = array();
-foreach($rs as $v){
-	$v->image = getFullAvatar($v->image);
-	$joiners[] = $v;
-}
-
-$joinerCount = count($joiners);
-
-if(strtotime($groupon['end_time']) < $time){//已结束
-
-}else{//进行中
-	if($joinerCount < $groupon['num']){//人数未满
-		
-	}
-}
-
-
-
-//商品信息
-$objPro = D('Product');
-$product = $objPro->getInfo($groupon['product_id']);
+$info = $info['result'];
+$time = $info['nowTime'];
+$info['beginDateline'] = strtotime($info['beginTime']);
+$info['endDateline'] = strtotime($info['endTime']);
+$info['remainSec'] = $info['endDateline'] - $time;
 
 include_once('tpl/groupon_join_web.php');
 ?>
