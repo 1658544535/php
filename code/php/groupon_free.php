39fd07de9f8083c80a000654ab599ed08e430257
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-28 03:42:51 +0000

    免团列表、拼团详情页面换用接口

--- code/php/groupon_free.php
+++ code/php/groupon_free.php
@@ -7,63 +7,68 @@ $ajax = isAjax();
 !$ajax && IS_USER_LOGIN();
 $time = time();
 
-$objFreeCpn = D('GrouponFreeCoupon');
-$freeCpn = $objFreeCpn->getUserCoupon($userid);
+//$objFreeCpn = D('GrouponFreeCoupon');
+//$freeCpn = $objFreeCpn->getUserCoupon($userid);
+//
+//if($ajax){
+//	if(empty($freeCpn)){
+//		ajaxResponse(false, '没有相关数据');
+//	}else{
+//		$page = intval($_REQUEST['page']);
+//		$page = max(1, $page);
+//		$perpage = 10;
+//		$objGroupon = M('groupon_activity');
+//		$rs = $objGroupon->gets(array('type'=>2,'status'=>1,'is_delete'=>0), 'id,product_id,title,price,banner', array('sorting'=>'desc','id'=>'desc'), $page, $perpage);
+//		$list = array();
+//		if(!empty($rs['DataSet'])){
+//			$activeIds = array();
+//			$productIds = array();
+//			foreach($rs['DataSet'] as $k => $v){
+//				$activeIds[] = $v->id;
+//				$productIds[] = $v->product_id;
+//			}
+//			$objPro = M('product');
+//			$pros = $objPro->getAll(array('__IN__'=>array('id'=>$productIds),'status'=>1), 'id,product_name,distribution_price,image_small');
+//			$proList = array();
+//			foreach($pros as $v){
+//				$proList[$v->id] = $v;
+//			}
+//			foreach($rs['DataSet'] as $k => $v){
+//				$tmp = array(
+//					'id' => $v->id,
+//					'name' => $v->title,
+//					'price' => $v->price,
+//					'oldPrice' => isset($proList[$v->product_id]) ? $proList[$v->product_id]->distribution_price : 0,
+//				);
+//				if(isset($proList[$v->product_id])){
+//					$tmp['name'] = $proList[$v->product_id]->product_name;
+//					$tmp['oldPrice'] = $proList[$v->product_id]->distribution_price;
+//					$tmp['imgSrc'] = $site_image.$proList[$v->product_id]->image_small;
+//				}else{
+//					$tmp['name'] = '';
+//					$tmp['oldPrice'] = 0;
+//					$tmp['imgSrc'] = '';
+//				}
+//				$list[] = $tmp;
+//			}
+//		}
+//
+//		$data = array(
+//			'data' => array(
+//				'pageNow' => $rs['CurrentPage'],
+//				'pageCount' => $rs['PageCount'],
+//				'listData' => $list,
+//			),
+//		);
+//		ajaxResponse(true, '', $data);
+//	}
+//}else{
+//	empty($freeCpn) && redirect(getPrevUrl());
+//	include_once('tpl/groupon_free_web.php');
+//}
 
-if($ajax){
-	if(empty($freeCpn)){
-		ajaxResponse(false, '没有相关数据');
-	}else{
-		$page = intval($_REQUEST['page']);
-		$page = max(1, $page);
-		$perpage = 10;
-		$objGroupon = M('groupon_activity');
-		$rs = $objGroupon->gets(array('type'=>2,'status'=>1,'is_delete'=>0), 'id,product_id,title,price,banner', array('sorting'=>'desc','id'=>'desc'), $page, $perpage);
-		$list = array();
-		if(!empty($rs['DataSet'])){
-			$activeIds = array();
-			$productIds = array();
-			foreach($rs['DataSet'] as $k => $v){
-				$activeIds[] = $v->id;
-				$productIds[] = $v->product_id;
-			}
-			$objPro = M('product');
-			$pros = $objPro->getAll(array('__IN__'=>array('id'=>$productIds),'status'=>1), 'id,product_name,distribution_price,image_small');
-			$proList = array();
-			foreach($pros as $v){
-				$proList[$v->id] = $v;
-			}
-			foreach($rs['DataSet'] as $k => $v){
-				$tmp = array(
-					'id' => $v->id,
-					'name' => $v->title,
-					'price' => $v->price,
-					'oldPrice' => isset($proList[$v->product_id]) ? $proList[$v->product_id]->distribution_price : 0,
-				);
-				if(isset($proList[$v->product_id])){
-					$tmp['name'] = $proList[$v->product_id]->product_name;
-					$tmp['oldPrice'] = $proList[$v->product_id]->distribution_price;
-					$tmp['imgSrc'] = $site_image.$proList[$v->product_id]->image_small;
-				}else{
-					$tmp['name'] = '';
-					$tmp['oldPrice'] = 0;
-					$tmp['imgSrc'] = '';
-				}
-				$list[] = $tmp;
-			}
-		}
+$freeCpn = apiData('checkGroupFreeApi.do', array('userId'=>$userid));
+empty($freeCpn) && redirect(getPrevUrl());
 
-		$data = array(
-			'data' => array(
-				'pageNow' => $rs['CurrentPage'],
-				'pageCount' => $rs['PageCount'],
-				'listData' => $list,
-			),
-		);
-		ajaxResponse(true, '', $data);
-	}
-}else{
-	empty($freeCpn) && redirect(getPrevUrl());
-	include_once('tpl/groupon_free_web.php');
-}
+include_once('tpl/groupon_free_web.php');
 ?>
--- code/php/groupon_multi.php
+++ code/php/groupon_multi.php
@@ -3,57 +3,61 @@ define('HN1', true);
 require_once('./global.php');
 
 $productId = intval($_GET['id']);
-$objGroupon = M('groupon_activity');
+//$objGroupon = M('groupon_activity');
+//
+//$objPro = D('Product');
+//$product = $objPro->get(array('id'=>$productId,'status'=>1), 'product_name,image_small', ARRAY_A);
+//
+//if(isAjax()){
+//	empty($productId) && ajaxResponse(false, '参数错误');
+//
+//	$page = intval($_REQUEST['page']);
+//	$page = max(1, $page);
+//	$perpage = 10;
+//
+//	$rs = $objGroupon->gets(array('product_id'=>$productId,'type'=>1,'status'=>1,'is_delete'=>0), 'id,product_id,title,price,banner,num', array('sorting'=>'desc','id'=>'desc'), $page, $perpage);
+//
+//	$list = array();
+//	if(!empty($rs['DataSet'])){
+//		$activeIds = array();
+//		foreach($rs['DataSet'] as $k => $v){
+//			$activeIds[] = $v->id;
+//		}
+//
+//		$sql = 'SELECT `activity_id`,COUNT(*) AS num FROM `groupon_user_record` WHERE `activity_type`=1 AND `activity_id` IN ('.implode(',', $activeIds).') GROUP BY `activity_id`';
+//		$records = $objGroupon->query($sql);
+//		$recordList = array();
+//		foreach($records as $v){
+//			$recordList[$v->activity_id] = $v->num;
+//		}
+//
+//		foreach($rs['DataSet'] as $k => $v){
+//			$list[] = array(
+//				'id' => $v->id,
+//				'name' => $product['product_name'],
+//				'price' => $v->price,
+//				'imgSrc' => $site_image.$product['image_small'],
+//				'num' => $v->num,
+//				'sales' => isset($recordList[$v->id]) ? $recordList[$v->id] : 0,
+//			);
+//		}
+//	}
+//
+//	$Data =array(
+//        'pageNow' => $rs['CurrentPage'],
+//		'pageCount' => $rs['PageCount'],
+//		'data' => $list
+//	);
+//	echo get_json_data_public(1, "获取成功", $Data);
+//}else{
+//	empty($productId) && redirect('/');
+//
+//	empty($product) && redirect(getPrevUrl(), '商品已下架');
+//	
+//	include_once('tpl/groupon_multi_web.php');
+//}
 
-$objPro = D('Product');
-$product = $objPro->get(array('id'=>$productId,'status'=>1), 'product_name,image_small', ARRAY_A);
 
-if(isAjax()){
-	empty($productId) && ajaxResponse(false, '参数错误');
 
-	$page = intval($_REQUEST['page']);
-	$page = max(1, $page);
-	$perpage = 10;
-
-	$rs = $objGroupon->gets(array('product_id'=>$productId,'type'=>1,'status'=>1,'is_delete'=>0), 'id,product_id,title,price,banner,num', array('sorting'=>'desc','id'=>'desc'), $page, $perpage);
-
-	$list = array();
-	if(!empty($rs['DataSet'])){
-		$activeIds = array();
-		foreach($rs['DataSet'] as $k => $v){
-			$activeIds[] = $v->id;
-		}
-
-		$sql = 'SELECT `activity_id`,COUNT(*) AS num FROM `groupon_user_record` WHERE `activity_type`=1 AND `activity_id` IN ('.implode(',', $activeIds).') GROUP BY `activity_id`';
-		$records = $objGroupon->query($sql);
-		$recordList = array();
-		foreach($records as $v){
-			$recordList[$v->activity_id] = $v->num;
-		}
-
-		foreach($rs['DataSet'] as $k => $v){
-			$list[] = array(
-				'id' => $v->id,
-				'name' => $product['product_name'],
-				'price' => $v->price,
-				'imgSrc' => $site_image.$product['image_small'],
-				'num' => $v->num,
-				'sales' => isset($recordList[$v->id]) ? $recordList[$v->id] : 0,
-			);
-		}
-	}
-
-	$Data =array(
-        'pageNow' => $rs['CurrentPage'],
-		'pageCount' => $rs['PageCount'],
-		'data' => $list
-	);
-	echo get_json_data_public(1, "获取成功", $Data);
-}else{
-	empty($productId) && redirect('/');
-
-	empty($product) && redirect(getPrevUrl(), '商品已下架');
-	
-	include_once('tpl/groupon_multi_web.php');
-}
+include_once('tpl/groupon_multi_web.php');
 ?>
