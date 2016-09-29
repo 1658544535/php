9ef3228ec1eca7ca38395de5a523886a3d415e49
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-29 15:17:45 +0000

    帐号调用接口

---
+++ tpl/wxuser_reg_web.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $site_name;?></title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=0" name="viewport">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="stylesheet" href="css/sm.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="js/swiper/swiper.min.css">
+    <script type='text/javascript' src='js/zepto.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/baiduTemplate.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/sui/sm.min.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/swiper/swiper.min.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/app.min.js' charset='utf-8'></script>
+	<script src="http://static.geetest.com/static/tools/gt.js"></script>
+</head>
+
+<body>
+    <div class="page-group" id="page-login">
+        <div id="page-nav-bar" class="page page-current">
+            <header class="bar bar-nav">
+                <a class="button button-link button-nav pull-left back" href="javascript:history.back(-1);">
+                    <span class="icon icon-back"></span>
+                </a>
+                <h1 class="title">注册</h1>
+            </header>
+
+            <div class="content">
+                <form action="wxuser_reg.php" method="post" onsubmit="return tgSubmit()">
+					<input type="hidden" name="openid" value="<?php echo $openid;?>" />
+                    <section class="edit-list-icon">
+                        <ul class="list">
+                            <li>
+                                <i class="icon icon-tel"></i>
+                                <div class="input-controll"><input type="text" name="mobile" id="mobile" class="txt" placeholder="手机号码" /></div>
+                            </li>
+							<li>
+                                <i class="icon icon-tel"></i>
+                                <div class="input-controll"><input type="password" name="password" id="password" class="txt" placeholder="密码" /></div>
+                            </li>
+							<li>
+                                <i class="icon icon-tel"></i>
+                                <div class="input-controll"><input type="password" name="repassword" id="repassword" class="txt" placeholder="确认密码" /></div>
+                            </li>
+
+                            <li>
+                                <i class="icon icon-code"></i>
+                                <div class="input-controll"><input type="text" name="code" id="code" class="txt" placeholder="验证码" /></div>
+                                <a href="javascript:;" class="btn" id="JS-vcodebtn" onclick="show_captcha()">发送验证码</a>
+								<span id='tip' class="btn btn-gray">&nbsp;&nbsp;<span id="time">0</span>&nbsp;秒后重发</span>
+                            </li>
+                        </ul>
+						<div id="captcha" style="margin:0 10px;"></div>
+                    </section>
+
+                    <div class="bin-btn">
+                        <input type="submit" value="注册" />
+                    </div>
+                </form>
+            </div>
+            
+
+        </div>
+    </div>
+	<script type="text/javascript">
+
+		function get_code( v_geetest_challenge, v_geetest_validate, v_geetest_seccode,captchaObj  )
+		{
+			$.ajax({
+				type: "POST",
+				dataType: "json",
+             	url: "/user_registered?act=get_code&tel=" + $("#mobile").val(),
+             	data:{
+	          			'geetest_challenge' :  v_geetest_challenge,
+						'geetest_validate'  :  v_geetest_validate,
+						'geetest_seccode'   :  v_geetest_seccode
+	          	},
+             	success: function(data){
+             		if(data.code > 0){
+						$.toast("验证码发送成功！");
+         				$(".code_btn").hide();
+         				$("#tip").show();
+         				getclock();
+         				$("#captcha").empty();
+             		}else{
+						$.toast(data.msg);
+             			captchaObj.refresh();
+             			// show_captcha();
+             		}
+                }
+			})
+		}
+
+		// 计时器
+		function getclock()
+		{
+			var seconds = 0;
+			var time = setInterval(function(){
+				seconds += 1;
+				document.getElementById('time').innerHTML = 60-seconds;
+
+				if (seconds == 60)
+				{
+					$(".code_btn").show();
+             		$("#tip").hide();
+             		document.getElementById('time').innerHTML = 60;
+             		 clearTimeout(time);
+				}
+			},1000);
+		}
+
+		function tgSubmit()
+		{
+			if($.trim($("#mobile").val()) == "")
+			{
+				$.toast("请输入帐号");
+				return false;
+			}
+
+			var pwd = $.trim($("#password").val());
+			var repwd = $.trim($("#repassword").val());
+			if(pwd == ""){
+				$.toast("请输入密码");
+				return false;
+			}
+			if(repwd == ""){
+				$.toast("请确认密码");
+				return false;
+			}
+			if(pwd != repwd){
+				$.toast("两次输入的密码不一致");
+				return false;
+			}
+
+			if($.trim($("#code").val()) == "")
+			{
+				$.toast("请输入验证码");
+				return false;
+			}
+
+			return true;
+		}
+
+
+		//验证码
+		function handler(captchaObj) {
+			$("#captcha").empty();
+	        // 将验证码加到id为captcha的元素里
+	        // captchaObj.appendTo("#captcha");
+	        captchaObj.appendTo("#captcha");
+
+	        captchaObj.onSuccess(function() {
+				var v_geetest_challenge = $('.geetest_challenge').val();
+	        	var v_geetest_validate = $('.geetest_validate').val();
+	        	var v_geetest_seccode = $('.geetest_seccode').val();
+
+	        	get_code( v_geetest_challenge, v_geetest_validate, v_geetest_seccode, captchaObj );
+	        });
+	    };
+	    var xhr = "";
+	    function show_captcha(){
+			var mobile = $.trim($("#mobile").val());
+			if(mobile == ""){
+				$.toast("请输入手机号");
+				return false;
+			}else if(!_checkMobile(mobile)){
+				$.toast("请正确输入手机号");
+				return false;
+			}
+	     	if(!!xhr){
+	     		$("#captcha").html("");
+				xhr.abort();		//取消未请求完成的ajax
+			}
+	     	xhr = $.ajax({
+		        // 获取id，challenge，success（是否启用failback）
+		        url: "/validate.php?act=init_geetest",
+		        type: "get",
+		        dataType: "json", // 使用jsonp格式
+		        success: function (data) {
+		            // 使用initGeetest接口
+		            // 参数1：配置参数，与创建Geetest实例时接受的参数一致
+		            // 参数2：回调，回调的第一个参数验证码对象，之后可以使用它做appendTo之类的事件
+		            $("#captcha").html("");
+		            initGeetest({
+		                gt: data.gt,
+		                challenge: data.challenge,
+		                product: "embed", // 产品形式embed
+		                offline: !data.success,
+		                width: "100%"
+		            }, handler);
+		        }
+		    });
+	    }
+
+		function _checkMobile(_mobile){
+			var re = /^1\d{10}/;
+			return re.test(_mobile);
+		}
+	</script>
+</body>
+
+</html>
