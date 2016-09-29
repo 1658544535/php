bbeac72dcde50a97b21fdcae63fa7a3318435f4a
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-09-29 13:23:13 +0000

    调整下单

--- order_common.php
+++ order_common.php
@@ -37,16 +37,18 @@
 
 if(isset($_SESSION['order']['address'])){
 	$address = $_SESSION['order']['address'];
+	$addrId = $address['id'];
 }else{
 	$address = apiData('defaultAddress.do', array('uid'=>$userid));
 	$address = $address['success'] ? $address['result'] : array();
+	$addrId = $address['addId'];
 }
 
 //地址是否可配送
 $canDispatch = (!empty($address) && in_array($address['province'], $unSendProviceIds)) ? false : true;
 
 $_SESSION['order']['productId'] = $productId;
-$_SESSION['order']['addressId'] = $address['id'];
+$_SESSION['order']['addressId'] = $addrId;
 
 include_once('tpl/order_web.php');
 ?>
