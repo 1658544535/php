cff8e43bad0a7c347daab741a75f4a6391c06830
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-10-10 12:21:55 +0000

    调整

--- code/php/tpl/aftersale_web.php
+++ code/php/tpl/aftersale_web.php
@@ -57,10 +57,8 @@
                         <div class="u-g-1">
                             <span class="type">拼团商品</span>
                             <span class="state">
-								<%if(data["data"][i].refundStatus == 0){%>
-									待审核
-								<%}else if(data["data"][i].refundStatus == 1){%>
-									审核通过，退款处理中
+								<%if(data["data"][i].refundStatus == 1){%>
+									审核中
 								<%}else if(data["data"][i].refundStatus == 2){%>
 									审核通过，请退货
 								<%}else if(data["data"][i].refundStatus == 3){%>
