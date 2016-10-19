ec240954c0c16dd7ef039f8e5d133d334ff08cdd
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-10-19 08:12:13 +0000

    支付

--- wxpay/pay.php
+++ wxpay/pay.php
@@ -6,68 +6,118 @@ define('HN1', true);
 require_once('../global.php');
 define('CUR_DIR_PATH', dirname(__FILE__));
 
-$orderId = (isset($_GET['oid']) && !empty($_GET['oid'])) ? intval($_GET['oid']) : 0;
-empty($orderId) && redirect('/orders.php', '参数错误');
+$refUrl = '/pay_success.php?';
+$prevUrl = getPrevUrl();
 
-
-include_once(MODEL_DIR.'/OrderModel.class.php');
-$Order = new OrderModel($db, 'orders');
-$order = $Order->get(array('order_id'=>$orderId));
-empty($order) && redirect('/orders.php', '订单不存在');
-($order->is_pay == 1) && redirect('/orders.php', '订单已支付');
-
-//实付金额
-$total_pay = $order->pay_online-$order->discount_price-$order->discount_integral_price;
-
-$time = time();
-
-require_once "lib/WxPay.Api.php";
-require_once "pay_cls/WxPay.JsApiPay.php";
-require_once 'log.php';
-
-$logDir = LOG_DIR.'/wx/';
-!file_exists($logDir) && mkdir($logDir, 0777, true);
-$logFile = $logDir.'pay_'.date('Y-m-d', $time).'.log';
-
-//初始化日志
-$logHandler= new CLogFileHandler($logFile);
-//$log = Log::Init($logHandler, 15);
-
-$log = Log::Init($logHandler, 1);
-
-$log->DEBUG("应付金额为：" . $total_pay);
-
-
-
-//①、获取用户openid
-$tools = new JsApiPay();
-$openId = $tools->GetOpenid();
-
-//②、统一下单
-$input = new WxPayUnifiedOrder();
-$input->SetBody('产品购买');
-$input->SetOut_trade_no(genWXPayOutTradeNo());
-$input->SetTotal_fee(($total_pay)*100);
-$input->SetTime_start(date('YmdHis', $time));
-$input->SetNotify_url($gSetting['site_url'].$gSetting['wx_pay_dir'].'notify.php');
-$input->SetTrade_type("JSAPI");
-$input->SetOpenid($openId);
-$input->SetAttach($order->order_id.'|'.$order->order_number);
-$unifiedOrder = WxPayApi::unifiedOrder($input);
-$jsApiParameters = $tools->GetJsApiParameters($unifiedOrder);
-
-function genWXPayOutTradeNo(){
-    list($sec, $usec) = explode(" ", microtime());
-    $sec = $sec * 1000000;
-    return date('YmdHis', time()).intval($sec);
+$orderId = intval($_GET['oid']);
+if(empty($orderId)){//下单直接支付
+	$payParam = array(
+		'appid' => trim($_GET['appid']),
+		'timestamp' => trim($_GET['timestamp']),
+		'noncestr' => trim($_GET['noncestr']),
+		'prepayid' => trim($_GET['prepayid']),
+		'sign' => trim($_GET['sign']),
+		'out_trade_no' => trim($_GET['outno']),
+	);
+}else{
+	$orderInfo = apiData('orderdetail.do', array('oid'=>$orderId));
+	!$orderInfo['success'] && redirect($prevUrl, $orderInfo['error_msg']);
+	$orderInfo = $orderInfo['result'];
+	$result = apiData('payOrder.do', array('orderNo'=>$orderInfo['orderInfo']['orderNo'],'payMethod'=>8,'uid'=>$userid));
+	!$result['success'] && redirect($prevUrl, $result['error_msg']);
+	$payParam = $result['result']['wxpay'];
 }
+
+//if(!empty($_SERVER['HTTP_REFERER'])){
+//	$refInfo = pathinfo($_SERVER['HTTP_REFERER']);
+//	if($refInfo['filename'] == 'order'){//下单后直接支付
+//		$prevUrl = !$result['success'] ? '/user_orders.php' : 'pay_success.php?outno='.$payParam['out_trade_no'];
+//	}else{
+//		$prevUrl = $_SERVER['HTTP_REFERER'];
+//	}
+//}
+!$result['success'] && redirect($prevUrl, $result['error_msg']);
+
+
+
+//使用接口获取JS支付所需的数据
+include_once('./lib/WxPay.Data.php');
+
+$jsapi = new WxPayJsApiPay();
+$jsapi->SetAppid($payParam['appid']);
+$jsapi->SetTimeStamp($payParam['timestamp']);
+$jsapi->SetNonceStr($payParam['noncestr']);
+$jsapi->SetPackage('prepay_id='.$payParam['prepayid']);
+$jsapi->SetSignType("MD5");
+$jsapi->SetPaySign($payParam['sign']);
+$jsApiParameters = json_encode($jsapi->GetValues());
+
+
+
+
+
+//$orderId = (isset($_GET['oid']) && !empty($_GET['oid'])) ? intval($_GET['oid']) : 0;
+//empty($orderId) && redirect('/orders.php', '参数错误');
+//
+//
+//include_once(MODEL_DIR.'/OrderModel.class.php');
+//$Order = new OrderModel($db, 'orders');
+//$order = $Order->get(array('order_id'=>$orderId));
+//empty($order) && redirect('/orders.php', '订单不存在');
+//($order->is_pay == 1) && redirect('/orders.php', '订单已支付');
+//
+////实付金额
+//$total_pay = $order->pay_online-$order->discount_price-$order->discount_integral_price;
+//
+//$time = time();
+//
+//require_once "lib/WxPay.Api.php";
+//require_once "pay_cls/WxPay.JsApiPay.php";
+//require_once 'log.php';
+//
+//$logDir = LOG_DIR.'/wx/';
+//!file_exists($logDir) && mkdir($logDir, 0777, true);
+//$logFile = $logDir.'pay_'.date('Y-m-d', $time).'.log';
+//
+////初始化日志
+//$logHandler= new CLogFileHandler($logFile);
+////$log = Log::Init($logHandler, 15);
+//
+//$log = Log::Init($logHandler, 1);
+//
+//$log->DEBUG("应付金额为：" . $total_pay);
+//
+//
+//
+////①、获取用户openid
+//$tools = new JsApiPay();
+//$openId = $tools->GetOpenid();
+//
+////②、统一下单
+//$input = new WxPayUnifiedOrder();
+//$input->SetBody('产品购买');
+//$input->SetOut_trade_no(genWXPayOutTradeNo());
+//$input->SetTotal_fee(($total_pay)*100);
+//$input->SetTime_start(date('YmdHis', $time));
+//$input->SetNotify_url($gSetting['site_url'].$gSetting['wx_pay_dir'].'notify.php');
+//$input->SetTrade_type("JSAPI");
+//$input->SetOpenid($openId);
+//$input->SetAttach($order->order_id.'|'.$order->order_number);
+//$unifiedOrder = WxPayApi::unifiedOrder($input);
+//$jsApiParameters = $tools->GetJsApiParameters($unifiedOrder);
+//
+//function genWXPayOutTradeNo(){
+//    list($sec, $usec) = explode(" ", microtime());
+//    $sec = $sec * 1000000;
+//    return date('YmdHis', time()).intval($sec);
+//}
 ?>
 <!doctype html>
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <title><?php echo $gSetting['site_name'];?>-支付</title>
+    <title><?php echo $site_name;?>-支付</title>
     <script type="text/javascript">
         //调用微信JS api 支付
         function jsApiCall()
@@ -78,11 +128,11 @@ function genWXPayOutTradeNo(){
                 function(res){
                     switch(res.err_msg){
                         case "get_brand_wcpay_request:ok":
-                            location.href='<?php echo $site;?>success_pay.php?order_id=<?php echo $order_id;?>';
+                            location.href = "<?php echo $refUrl;?>&state=1&outno=<?php echo $payParam['out_trade_no'];?>";
                             break;
                         case "get_brand_wcpay_request:cancel":
                         case "get_brand_wcpay_request:fail":
-                            location.href = "<?php echo $site;?>orders.php";
+                            location.href = "<?php echo $refUrl;?>&state=0";
                             break;
                     }
                 }
