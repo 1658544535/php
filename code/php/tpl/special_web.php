4920b6a61616a0bcbb86bf5940e4990ae7b42b2d
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-10-22 08:06:13 +0000

    添加专题分享文字

--- code/php/tpl/special_web.php
+++ code/php/tpl/special_web.php
@@ -22,7 +22,7 @@
 	var imgUrl = "<?php echo $pic['image'];?>";
 	var link  = "<?php echo $site;?>special.php?id=<?php echo $id;?>";
 	var title ="<?php echo $pic['specialName'];?>";
-	var desc  ="";
+	var desc  ="【拼得好】缤纷专题好货，点击拼手速立抢！";
 	wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam['timestamp'];?>, '<?php echo $wxShareParam['nonceStr'];?>', '<?php echo $wxShareParam['signature'];?>', imgUrl,link,title,desc);
 	</script>
 </head>
