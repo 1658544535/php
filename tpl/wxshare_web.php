16301eeace14ae806e922dd1c8ae4f18f9b3dab0
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-10-22 12:08:51 +0000

    增加默认分享公共脚本

---
+++ tpl/wxshare_web.php
@@ -0,0 +1,9 @@
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script type="text/javascript" src="/js/wxshare.js"></script>
+<script type="text/javascript">
+var imgUrl = "<?php echo $site;?>images/wxLOGO.png";
+var link = "<?php echo $site;?>";
+var title ="<?php echo $site_name;?>";
+var desc = "拼得精彩";
+wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam['timestamp'];?>, '<?php echo $wxShareParam['nonceStr'];?>', '<?php echo $wxShareParam['signature'];?>', imgUrl, link, title, desc);
+</script>
