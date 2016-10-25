2b097bcc729bbdd87a8ea6d821d221abad9e2227
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-10-25 06:40:22 +0000

    优惠券分享

---
+++ code/php/coupon_action.php
@@ -0,0 +1,21 @@
+<?php
+define('HN1', true);
+require_once('./global.php');
+IS_USER_LOGIN();
+    $linkid  = CheckDatas( 'linkid', '' );
+    $Aid     = CheckDatas( 'aid', '' );
+	$coupon = apiData('getCouponApi.do',array('linkId'=>$linkid,'userId'=>$userid));
+   if($Aid !=''){
+    if($coupon['success']){
+	    redirect('groupon.php&id='.$Aid,领取成功！);
+    }else{
+    	redirect('index.php',$coupon['error_msg']);
+    }
+   }else{
+   	if($coupon['success']){
+   		redirect('index.php',领取成功！);
+   	}else{
+   		redirect('index.php',$coupon['error_msg']);
+   	}
+   }
+?>
