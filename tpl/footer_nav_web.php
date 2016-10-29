fffa92fec67fc7b2cec8197fe77b37443e196b5f
xujiakai <xujiakai@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-10-29 07:27:34 +0000

    首页调整

--- tpl/footer_nav_web.php
+++ tpl/footer_nav_web.php
@@ -3,13 +3,17 @@
 		<span class="icon i-home"></span>
 		<span class="tab-label">首页</span>
 	</a>
-	<!--  <a class="tab-item <?php echo ($footerNavActive=='spe77')?'active':'';?>" href="special_77.php">-->
-<!-- 		<span class="icon i-price"></span> -->
-<!-- 		<span class="tab-label">7.7专区</span> -->
-<!-- 	</a> -->
-	<a class="tab-item <?php echo ($footerNavActive=='guess')?'active':'';?>" href="product_guess_price.php">
+	<!-- <a class="tab-item <?php echo ($footerNavActive=='spe77')?'active':'';?>" href="special_77.php">
+		<span class="icon i-price"></span>
+		<span class="tab-label">7.7专区</span>
+	</a> -->
+	<!-- <a class="tab-item <?php echo ($footerNavActive=='guess')?'active':'';?>" href="product_guess_price.php">
 		<span class="icon i-price"></span>
 		<span class="tab-label">猜价格</span>
+	</a> -->
+	<a class="tab-item" href="special.php?id=6">
+		<span class="icon i-new"></span>
+		<span class="tab-label">新品</span>
 	</a>
 	<a class="tab-item <?php echo ($footerNavActive=='search')?'active':'';?>" href="search.php">
 		<span class="icon i-search"></span>
