6555635a995cbe2755e3d47ad7290265633e875e
dushaojin <dushaojin@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-11-03 06:07:20 +0000

    猜价格 往期

---
+++ ajaxtpl/ajax_product_guess_before_price.php
@@ -0,0 +1,68 @@
+<?php
+define('HN1', true);
+require_once('../global.php');
+
+$GrouponActivityModel         = M('groupon_activity');
+$GrouponActivityRecordModel   = M('groupon_activity_record');
+$GrouponUserRecordModel       = M('groupon_user_record');
+$FocusSettingModel            = M('focus_setting');
+$UserInfoModel 				  = M('user_info');
+$ProductFocusImagesModel      = M('product_focus_images');
+
+$act  = CheckDatas( 'act', 'info' );
+$productId   	= CheckDatas( 'pid', '' );
+$uId   	        = CheckDatas( 'uid', '' );
+$gId   	        = CheckDatas( 'gid', '' );
+$Prize   	    = CheckDatas( 'type', '' );
+$Price          = CheckDatas( 'price', '' );
+$as             = CheckDatas( 'activity_status', '' );
+$page           = max(1, intval($_POST['page']));
+
+switch($act)
+{
+    case 'user':
+    	//获取更多用户参与信息数据
+        $ObjUserList    = apiData('userJoinInfoApi.do', array('activityId'=>$gId,'pageNo'=>$page,'pageSize'=>20));
+
+        if($ObjUserList !='')
+        {
+        	echo ajaxJson( 1,'获取成功',$ObjUserList['result']['joinUserList'],$page);
+        }
+        else
+        {
+        	echo ajaxJson( 0,'获取失败');
+        
+        }
+    	break;
+
+    case 'prize':
+    	//获取更多中奖用户信息数据
+    	$ObjPrizeList    = apiData('winListApi.do', array('activityId'=>$gId,'pageNo'=>$page,'prize'=>$Prize));
+
+        if($ObjPrizeList !='')
+        {
+        	echo ajaxJson( 1,'获取成功',$ObjPrizeList['result']['prizeList'],$page);
+        }
+        else
+        {
+        	echo ajaxJson( 0,'获取失败');
+        }
+    	break;
+    
+    default:
+	    //往期活动列表
+	    $ObjGrouponList = apiData('guessBeforeActivityApi.do', array('pageNo'=>$page));
+		$Data =array(
+		    'data'=>$ObjGrouponList['result'],
+        );
+
+        if ($ObjGrouponList !='') {
+            echo get_json_data_public( 1,'获取成功',$Data ,$page);
+        } else {
+            echo get_json_data_public( 0,'获取失败' );
+        }
+		 	
+}
+
+
+?>
---
+++ tpl/product_guess_before_price_list_web.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $site_name;?></title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=0" name="viewport">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="stylesheet" href="css/sm.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="js/swiper/swiper.min.css">
+    <script type='text/javascript' src='js/zepto.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/baiduTemplate.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/sui/sm.min.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/swiper/swiper.min.js' charset='utf-8'></script>
+    <script type='text/javascript' src='js/app.min.js' charset='utf-8'></script>
+    <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+	<script type="text/javascript" src="/js/wxshare.js"></script>
+	<script type="text/javascript">
+	var imgUrl = "<?php echo $fx['image'];?>";
+	var link   = "<?php echo $fx['url'];?>";
+	var title  = "<?php echo $fx['title'];?>";
+	var desc   = "<?php echo $fx['content'];?>";
+	wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam['timestamp'];?>, '<?php echo $wxShareParam['nonceStr'];?>', '<?php echo $wxShareParam['signature'];?>', imgUrl, link, title,desc);
+	</script>
+</head>
+
+<body>
+    <div class="page-group" id="page-guessList">
+        <div id="page-nav-bar" class="page page-current">
+            <header class="bar bar-nav">
+                <a class="button button-link button-nav pull-left back" href="javascript:history.back(-1);">
+                    <span class="icon icon-back"></span>
+                </a>
+                <h1 class="title">猜价赢好礼</h1>
+            </header>
+            <nav class="bar bar-tab">
+                <a href="product_guess_price.php" class="tab-item tab-item2">
+                    <span class="icon i-lotterying"></span>
+                    <span class="tab-label">进行中</span>
+                </a>
+                <a class="tab-item active">
+                    <span class="icon i-lotteryed"></span>
+                    <span class="tab-label">查看往期</span>
+                </a>
+            </nav>
+
+            <div class="content native-scroll">
+
+                <section class="guessList pullbox infinite-scroll infinite-scroll-bottom" data-distance="30" data-href="ajaxtpl/ajax_product_guess_before_price.php">
+                    <ul class="list-container"></ul>
+                    <!-- 加载提示符 -->
+                    <div class="infinite-scroll-preloader">
+                        <div class="preloader"></div>
+                    </div>
+                </section>
+
+            </div>
+
+           <script id='tpl_pull' type="text/template">
+            <%if(data["data"].length>0){%>
+                <%for(var i=0;i<data["data"].length; i++){%>
+                    <li><a href="product_guess_price.php?act=detail&gid=<%=data["data"][i]["activityId"]%>&pid=<%=data["data"][i]["productId"]%>">
+                        <div class="img"><img src="<%=data["data"][i]["productImage"]%>" /></div>
+                        <div class="info">
+                            <div class="name"><%=data["data"][i]["productName"]%></div>
+                            <div class="time">
+                                <span class="btn">
+                                    <%if(data["data"][i]["isPrize"] == 2){%>已开奖<%}%>
+                                    <%if(data["data"][i]["isPrize"] == 1){%>待开奖<%}%>
+                                </span>
+                                <!--<span class="btn">待开奖</span>-->
+                            </div>
+                            <div class="tips">提示区间：<%=data["data"][i]["minPrice"]%>-<%=data["data"][i]["maxPrice"]%> 丨 已有<span><%=data["data"][i]["joinNum"]%></span>人参与</div>
+                        </div>
+                    </a></li>
+                <%}%>
+            <%}else{%>
+                <div class="tips-null">暂无商品</div>
+            <%}%>
+            </script>
+        </div>
+
+        <div class="popup popup-rule">
+            <a href="#" class="close-popup"></a>
+        </div>
+        <section id="goTop" class="goTop"></section>
+    </div>
+   
+</body>
+
+</html>
