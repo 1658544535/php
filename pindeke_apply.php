f4b35637e7d5739b8cd5d1e80d4b7430e5fa91c9
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-11-11 03:53:21 +0000

    拼得客修改信息上传图片问题BUG修复

--- pindeke_apply.php
+++ pindeke_apply.php
@@ -74,27 +74,25 @@ switch($act){
 	     $Id  = intval($_GET['id']);
 	  
 	  
-	if(IS_POST()){
-			set_time_limit(0);
-			$apiParam = array();
-			$upImgs = $_POST['img'];
-			$i = 1;
-			foreach($upImgs as $v){
-				$apiParam['image'.$i] = $v;
-				$i++;
-			}
-			
-			$apiParam['cardNo']     = $cardNo;
-			$apiParam['extChannel'] = $Content;
-			$apiParam['name']       = $Name;
-			$apiParam['userId']     = $userid;
-			$apiParam['phone']      = $Phone;
+			if(IS_POST()){
+					set_time_limit(0);
+					$apiParam = array();
+					$upImgs = $_POST['img'];
+					$i = 1;
+					foreach($upImgs as $v){
+						$apiParam['imgName'.$i] = $v;
+						$i++;
+					}
+					
+					$apiParam['cardNo']     = $cardNo;
+					$apiParam['extChannel'] = $Content;
+					$apiParam['name']       = $Name;
+					$apiParam['userId']     = $userid;
+					$apiParam['id']         = $Id;
+					$apiParam['phone']      = $Phone;
+		
+					$result = apiData('pdkUpdateApi.do',$apiParam,'post');
 
-			$result = apiData('pdkApplyApi.do',$apiParam,'post',true);
-
-			
-			
-			
 			
 			if(empty($_SESSION['backurl_aftersale'])){
 				$prevUrl = '/';
@@ -114,51 +112,9 @@ switch($act){
 			
 			
 			
-// 			if($_POST['img1'] !=''){
-// 				$upImgs1 = '@'.IMAGE_UPLOAD_DIR.$_POST['img1'];
-// 			}elseif($_POST['img1'] =='' && $_POST['img_before1'] !=''){
-// 				$upImgs1 = $_POST['img_before1'];
-// 			}else{
-// 				$upImgs1 ='';
-// 			}
 			
-// 			if($_POST['img2'] !=''){
-// 				$upImgs2 = '@'.IMAGE_UPLOAD_DIR.$_POST['img2'];
-// 			}elseif($_POST['img2'] =='' && $_POST['img_before2'] !=''){
-// 				$upImgs2 = $_POST['img_before2'];
-// 			}else{
-// 				$upImgs2 ='';
-// 			}
-			
-// 			if($_POST['img3'] !=''){
-// 				$upImgs3 = '@'.IMAGE_UPLOAD_DIR.$_POST['img3'];
-// 			}elseif($_POST['img3'] =='' && $_POST['img_before3'] !=''){
-// 				$upImgs3 = $_POST['img_before3'];
-// 			}else{
-// 				$upImgs3 ='';
-// 			}
-			
-// 			if($_POST['img4'] !=''){
-// 				$upImgs4 = '@'.IMAGE_UPLOAD_DIR.$_POST['img4'];
-// 			}elseif($_POST['img4'] =='' && $_POST['img_before4'] !=''){
-// 				$upImgs4 = $_POST['img_before4'];
-// 			}else{
-// 				$upImgs4 ='';
-// 			}
-			
-// 			if($_POST['img5'] !=''){
-// 				$upImgs5 = '@'.IMAGE_UPLOAD_DIR.$_POST['img5'];
-// 			}elseif($_POST['img5'] =='' && $_POST['img_before5'] !=''){
-// 				$upImgs5 = $_POST['img_before5'];
-// 			}else{
-// 				$upImgs5 ='';
-// 			}
-			 
-// 			$result = apiData('pdkUpdateApi.do',array('cardNo'=>$cardNo,'id'=>$Id,'extChannel'=>$Content,'name'=>$Name,'userId'=>$Uid,'phone'=>$Phone,'image1'=>$upImgs1,'image2'=>$upImgs2,'image3'=>$upImgs3,'image4'=>$upImgs4,'image5'=>$upImgs5),'post',true);
-			
-			case 'uploadimg_edit'://上传图片
+			case 'uploadimg_edit'://信息修改上传图片
 				    $upfile = $_FILES['files'];
-			
 				    ($upfile['size'][0] <= 0) && ajaxResponse(false, '请选择图片');
 					$allowTypes = array('image/jpg','image/jpeg','image/png','image/pjpeg','image/gif','image/bmp','image/x-png');
 					!in_array($upfile['type'][0], $allowTypes) && ajaxResponse(false, '只能上传图片');
@@ -166,15 +122,12 @@ switch($act){
 					$userId = intval($_GET['uid']);
 					!file_exists(IMAGE_UPLOAD_DIR) && mkdir(IMAGE_UPLOAD_DIR, 0777, true);
 					$destFile = $userId.'_'.time().'.'.$fileInfo['extension'];
-					
 					$result = apiData('upUserpindekeImage.do',array('file'=>'@'.$upfile['tmp_name'][0]),'post');
-				
 					if($result['result'] !=''){
 						ajaxResponse(true, $result['result']);
 					}else{
 						ajaxResponse(false, $result['error_msg']);
 					}
-			
 				break;
 			
 			
@@ -189,7 +142,6 @@ switch($act){
 					$userId = intval($_GET['uid']);
 					!file_exists(IMAGE_UPLOAD_DIR) && mkdir(IMAGE_UPLOAD_DIR, 0777, true);
 					$destFile = $userId.'_'.time().'.'.$fileInfo['extension'];
-			
 					if(move_uploaded_file($upfile['tmp_name'][0], IMAGE_UPLOAD_DIR.$destFile)){
 						ajaxResponse(true, $destFile, array('url'=>IMAGE_UPLOAD_URL.$destFile));
 					}else{
