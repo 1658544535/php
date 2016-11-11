42abe3f1c6ee0aa0b14faf90b0fcaddd74cd7fef
dushaojin <dushaojin@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-11-11 08:57:12 +0000

    微信自定义导航菜单

--- code/php/admin/wx_menu.php
+++ code/php/admin/wx_menu.php
@@ -8,17 +8,51 @@
 
 define('HN1', true);
 define('LIB_ROOT', '../includes/lib/');
+define('DATA_DIR', dirname(__FILE__) . '/data/'); //数据保存的位置
 
 include_once(LIB_ROOT.'Weixin.class.php');
 include_once(LIB_ROOT.'weixin/errCode.php');
+
 $OptionWX = array(
     'token'=>'weixin', //填写你设定的key
     'encodingaeskey'=> '', //填写加密用的EncodingAESKey
     'appid'=>'wx3eea553d8ab21caa', //填写高级调用功能的app id
     'appsecret'=>'8a8eaaeda77febffb186e26e42572df6' //填写高级调用功能的密钥
 );
-$objWechat = new Wechat($OptionWX);
-$menus_arr = $objWechat->getMenu();
+
+$createMenu_file = "CreateMenu.json"; //保存的新建菜单JSON数组
+$localMenu_file  = "LocalMenu.json"; //本地尚未同步至微信的json数组
+$saveMenu_file   = "SaveMenu.json"; //保存已经创建的数组=> 通过getMenu()方法获取的数组
+
+//获取微信端当前Menu的配置,默认返回array
+function getWechatMenuData($OptionWX, $type = '') {
+    $objWechat = new Wechat($OptionWX);
+    $result = $objWechat->getMenu();
+    if ($type) {
+        $json = json_encode($result, JSON_UNESCAPED_UNICODE);
+        return $json;
+    }
+    return $result;
+}
+
+//判断是否存在本地配置缓存文件
+if (!file_exists(DATA_DIR . $localMenu_file)) {
+    $WechatMenuJSON = getWechatMenuData($OptionWX, true);
+    file_put_contents(DATA_DIR . $localMenu_file, $WechatMenuJSON); //创建缓存文件并写入
+}
+
+//$objWechat = new Wechat($OptionWX);
+$local_Menu_JSON = file_get_contents(DATA_DIR . $localMenu_file); //获取本地的菜单数组
+$local_Menu_Arr  = json_decode($local_Menu_JSON, true); //转换为php数组
+
+if (!$local_Menu_Arr) { //判断当前本地的数组是否为空
+    $WechatMenuJSON = getWechatMenuData($OptionWX, true);
+    file_put_contents(DATA_DIR . $localMenu_file, $WechatMenuJSON);//保存至本地文件
+} else {
+    $WechatMenuArr = $local_Menu_Arr;
+}
+
+$button_arr = $WechatMenuArr;
 
 $button_type_arr = array(
     '1'  => 'click',
@@ -42,61 +76,89 @@ $button_type_arr = array(
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="//cdn.bootcss.com/jquery/2.1.0/jquery.min.js"></script>
+    <script src="js/jquery.form.min.js"></script>
     <title>微信自定菜单</title>
 </head>
 <body>
 <div>
-    <form action="" method="post">
+
+    <form action="" method="post" id="wechatMenuForm">
         <ul>
 
-            <li data-id="1">
-                <div>
-                    名称：<input type="text" name="Pbutton_name_1">
+            <?php foreach ($button_arr as $k => $v) {
+//                print_r($v["sub_button"]);
+                ?>
 
+                <li data-id="<?php echo $k+1 ?>">
                     <div>
-                        类型：<select name="Pbutton_type_1" class="js-button-type" onchange="ButtonType(this);">
-                            <option value="">请选择按钮类型</option>
-                            <option value="sub">sub_button</option>
-                            <?php foreach ($button_type_arr as $k => $v) { ?>
-                                <option value="<?php echo $v; ?>"><?php echo $v; ?></option>
-                            <?php } ?>
-
-                        </select>
-                    </div>
-
-                    <div class="js-button-type-box">
-
+                        名称：<input type="text" name="Pbutton_name_<?php echo $k+1 ?>" value="<?php echo $v["name"];?>">
                         <div>
-                            链接：<input type="text" name="Pbutton_url_1">
+                            类型：<select name="Pbutton_type_<?php echo $k+1 ?>" class="js-button-type" onchange="ButtonType(this);">
+                                <option value="">请选择按钮类型</option>
+                                <option value="sub" <?php if (isset($v["sub_button"])) echo "selected";?>>sub_button</option>
+                                <?php foreach ($button_type_arr as $key => $value) {  ?>
+                                    <option value="<?php echo $value; ?>"  <?php if (!isset($v["sub_button"])) { if ($v["type"] == $value) echo "selected"; } ?>><?php echo $value; ?></option>
+                                <?php } ?>
+                            </select>
                         </div>
-                        <div>
-                            key ：<input type="text" name="Pbutton_key_1">
+                        <div class="js-button-type-box" style="<?php if (isset($v["sub_button"])) echo "display: none;"; ?>">
+                            <div>
+                                链接：<input type="text" name="Pbutton_url_<?php echo $k+1 ?>" value="<?php echo isset($v["url"]) ? $v["url"] : ''; ?>">
+                            </div>
+                            <div>
+                                key ：<input type="text" name="Pbutton_key_<?php echo $k+1 ?>" value="<?php echo isset($v["key"]) ? $v["key"] : ''; ?>">
+                            </div>
                         </div>
+                        <!--二级菜单-->
+                        <div style="<?php if (!isset($v["sub_button"])) echo "display: none;"; ?>">
+                            <ul>
 
+                                <?php foreach ($v as $sub_key => $sub_buttons) {
+                                    //循环输出二级菜单 ?>
+                                    <?php if (is_array($sub_buttons)){ ?>
+                                        <?php foreach ($sub_buttons as $sub_button) { ?>
+
+                                            <li>
+                                                <br>
+                                                <div> 名称：<input type="text" name="Cbutton_names_<?php echo $k+1 ?>[]" value="<?php echo $sub_button["name"]; ?>"> </div>
+                                                <div>
+                                                    类型：<select name="Cbutton_types_<?php echo $k+1 ?>[]">
+                                                        <?php foreach ($button_type_arr as $key => $value) { ?>
+                                                            <option value="<?php echo $value;?>" <?php if ($sub_button["type"] == $value) echo "selected"; ?>><?php echo $value;?></option>
+                                                        <?php } ?>
+                                                    </select>
+                                                </div>
+                                                <div> 链接：<input type="text" name="Cbutton_urls_<?php echo $k+1 ?>[]" value="<?php echo isset($sub_button["url"]) ? $sub_button["url"] : ''; ?>"> </div>
+                                                <div> key ：<input type="text" name="Cbutton_keys_<?php echo $k+1 ?>[]" value="<?php echo isset($sub_button["key"]) ? $sub_button["key"] : ''; ?>"> </div>
+                                            </li>
+
+                                        <?php } ?>
+                                    <?php } ?>
+                                <?php } ?>
+
+                                <a href="#" onclick="newChild(this);">点击添加</a>
+                            </ul>
+
+                        </div>
                     </div>
 
-                    <!--二级菜单-->
-                    <div style="display: none;">
-                        <ul>
-                            <a href="#" onclick="newChild(this);">点击添加</a>
-                        </ul>
-                    </div>
-                </div>
+                    <hr>
 
-            </li>
+                </li>
 
+            <?php } ?>
 
 
             <li>
-                <hr>
                 <a href="#" class="js-newFirst" onclick="newFirst(this);">点击添加</a>
             </li>
         </ul>
 
         <hr>
-
+        <input type="hidden" value="1" name="isCreatJSON">
         <button type="button" onclick="submit();">保存并提交</button>
     </form>
+
 </div>
 </body>
 <script>
@@ -143,7 +205,8 @@ $button_type_arr = array(
 
 <?php
 
-if (isset($_POST)){
+
+if (isset($_POST) && !empty($_POST)){
 
     $button_arr = array();
 
@@ -208,7 +271,7 @@ if (isset($_POST)){
 
             foreach ($_POST['Cbutton_names_'.'3'] as $k3 => $Cbutton_name_3) {
                 $sub_button_3[] = array(
-                    'name' => $Cbutton_name_3,
+                    'name ' => $Cbutton_name_3,
                     'type'  => $_POST['Cbutton_types_'.'3'][$k3],
                     'url'   => $_POST['Cbutton_urls_'.'3'][$k3],
                     'key'   => $_POST['Cbutton_keys_'.'3'][$k3],
@@ -232,24 +295,29 @@ if (isset($_POST)){
         'button' => $button_arr,
     );
 
-    if (isset($_POST['Pbutton_name_1'])) {
-        $result = $objWechat->createMenu($CreateNewMenu);
-        var_dump($objWechat->errMsg);
-        var_dump($objWechat->errCode);
-    }
-
-//    $result = $objWechat->createMenu($newmenu);
-//    $result = $objWechat->createMenu($CreateArr);
-//    $result = $objWechat->getMenu();
-
-//    print_r($result);
-
-//    var_dump($objWechat->errMsg);
-//    var_dump($objWechat->errCode);
-
+    $local_menu_JSON    = json_encode($button_arr, JSON_UNESCAPED_UNICODE);
+    $CreateNewMenu_JSON = json_encode($CreateNewMenu, JSON_UNESCAPED_UNICODE); //将生成php数组转为json数组
 }
 
-
+if (isset($_POST) && !empty($_POST)) {?>
+    <?php if (file_put_contents(DATA_DIR . $createMenu_file, $CreateNewMenu_JSON) && file_put_contents(DATA_DIR . $localMenu_file, $local_menu_JSON)) {?>
+        <h1>保存成功，是否需要提交同步至微信？</h1>
+        <form action="">
+            <button type="button" onclick="pushToWechat();">确定</button>
+        </form>
+        <script>
+            var token = 'HAHAHA';
+            function pushToWechat() {
+                var url = 'push_to_wechat.php?token=' + token ;
+                $.getJSON(url,function (data) {
+                    alert(data.info);
+                });
+            }
+        </script>
+    <?php } else { ?>
+        <h1>保存失败</h1>
+    <?php }?>
+<?php } ?>
 
 
 
