8e023defb541e6b1345395307be56be29faca363
xujiakai <xujiakai@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-11-16 12:46:28 +0000

    后台调整

--- code/php/admin/tpl/menu_web.php
+++ code/php/admin/tpl/menu_web.php
@@ -12,9 +12,7 @@
 <body>
 <div>
 
-    <a href="auth.php?act=logout">登出</a>
-    <a href="wx_reply.php">自定义回复</a>
-
+    <a href="wx_menu?act=logout">登出</a>
     <form action="wx_menu.php?act=update" method="post" id="wechatMenuForm">
         <ul>
 
@@ -33,45 +31,46 @@
                                 <?php } ?>
                             </select>
                         </div>
-                        <!--一级-->
-                        <div class="js-button-type-box" style="<?php if (isset($v["sub_button"]) && !empty($v["sub_button"])) echo "display: none;"; ?>">
-                            <div>
+                        <div class="js-button-type-box">
+                            <div class="js-click">
                                 链接：<input type="text" class="js-url" name="Pbutton_url_<?php echo $k+1 ?>" value="<?php echo isset($v["url"]) ? $v["url"] : ''; ?>">
                             </div>
-                            <div>
+                            <div class="js-view">
                                 key ：<input type="text" class="js-key" name="Pbutton_key_<?php echo $k+1 ?>" value="<?php echo isset($v["key"]) ? $v["key"] : ''; ?>">
                             </div>
-                        </div>
-                        <!--二级菜单-->
-                        <div style="<?php if (!isset($v["sub_button"]) && empty($v["sub_button"])) echo "display: none;"; ?>">
-                            <ul>
+                            <!--二级菜单-->
+                            <div class="js-sub" style="<?php if (!isset($v["sub_button"]) && empty($v["sub_button"])) echo "display: none;"; ?>">
+                                <ul>
 
-                                <?php foreach ($v as $sub_key => $sub_buttons) {
-                                    //循环输出二级菜单 ?>
-                                    <?php if (is_array($sub_buttons)){ ?>
-                                        <?php foreach ($sub_buttons as $sub_button) { ?>
+                                    <?php foreach ($v as $sub_key => $sub_buttons) {
+                                        //循环输出二级菜单 ?>
+                                        <?php if (is_array($sub_buttons)){ ?>
+                                            <?php foreach ($sub_buttons as $sub_button) { ?>
 
-                                            <li>
-                                                <a href="javascript:" class="js-delete">删除</a>
-                                                <div> 名称：<input type="text" name="Cbutton_names_<?php echo $k+1 ?>[]" value="<?php echo $sub_button["name"]; ?>"> </div>
-                                                <div>
-                                                    类型：<select name="Cbutton_types_<?php echo $k+1 ?>[]">
-                                                        <?php foreach ($button_type_arr as $key => $value) { ?>
-                                                            <option value="<?php echo $value;?>" <?php if ($sub_button["type"] == $value) echo "selected"; ?>><?php echo $value;?></option>
-                                                        <?php } ?>
-                                                    </select>
-                                                </div>
-                                                <div> 链接：<input type="text" name="Cbutton_urls_<?php echo $k+1 ?>[]" value="<?php echo isset($sub_button["url"]) ? $sub_button["url"] : ''; ?>"> </div>
-                                                <div> key ：<input type="text" name="Cbutton_keys_<?php echo $k+1 ?>[]" value="<?php echo isset($sub_button["key"]) ? $sub_button["key"] : ''; ?>"> </div>
-                                            </li>
+                                                <li>
+                                                    <a href="javascript:" class="js-delete">删除</a>
+                                                    <div> 名称：<input type="text" name="Cbutton_names_<?php echo $k+1 ?>[]" value="<?php echo $sub_button["name"]; ?>"> </div>
+                                                    <div>
+                                                        类型：<select onchange="ButtonType(this);" name="Cbutton_types_<?php echo $k+1 ?>[]">
+                                                            <?php foreach ($button_type_arr as $key => $value) { ?>
+                                                                <option value="<?php echo $key;?>" <?php if ($sub_button["type"] == $value) echo "selected"; ?>><?php echo $value;?></option>
+                                                            <?php } ?>
+                                                        </select>
+                                                    </div>
+                                                    <div>
+                                                        <div> 链接：<input type="text" name="Cbutton_urls_<?php echo $k+1 ?>[]" value="<?php echo isset($sub_button["url"]) ? $sub_button["url"] : ''; ?>"> </div>
+                                                        <div> key ：<input type="text" name="Cbutton_keys_<?php echo $k+1 ?>[]" value="<?php echo isset($sub_button["key"]) ? $sub_button["key"] : ''; ?>"> </div>
+                                                    </div>
+                                                </li>
 
+                                            <?php } ?>
                                         <?php } ?>
                                     <?php } ?>
-                                <?php } ?>
 
-                                <a href="#" onclick="newChild(this);">点击添加</a>
-                            </ul>
+                                    <a href="#" onclick="newChild(this);">点击添加</a>
+                                </ul>
 
+                            </div>
                         </div>
                     </div>
 
@@ -104,17 +103,26 @@
 
 </body>
 <script>
+    $(function(){
+        update_select();
+    });
+
     /* 判断BUTTON的种类是否为二级*/
     function ButtonType(_this) {
         var type = $(_this).val();
+        var subDiv = $(_this).parent().next().children();
+        var index = subDiv.length;
         /* 若为二级 */
         if (type == 'sub1' || type == 'sub2' || type == 'sub3' || type == 'sub' ) {
-            $(_this).parent().next().css("display", "none");
-            $(_this).parent().next().next().css("display", "block");
-        } else {
-            $(_this).parent().next().css("display", "block");
-            $(_this).parent().next().next().css("display", "none");
+            index = 2;
+        } else if(type == "click"){
+            index = 0;
+        }else if(type == "view"){
+            index = 1;
+        }else {
+            index = subDiv.length;
         }
+        subDiv.hide().eq(index).show();
     }
 
     /* 添加新的一级菜单 */
@@ -126,21 +134,23 @@
             $(".js-newFirst").hide();
             // return false;
         }
-        var html = '<li data-id="'+ i +'"><hr><a href="javascript:" class="js-delete" onclick="deleteFirst(this)">删除</a><div> 名称：<input type="text" name="Pbutton_name_'+ i +'"> <div> 类型：<select name="Pbutton_type_'+ i +'" class="js-button-type" onchange="ButtonType(this);"> <option value="">请选择按钮类型</option> <option value="sub">菜单</option><?php foreach ($button_type_arr as $k => $v) { ?><option value="<?php echo $k; ?>"><?php echo $v; ?></option><?php } ?></select> </div> <div class="js-button-type-box"> <div> 链接：<input type="text" name="Pbutton_url_'+ i +'"> </div> <div> key ：<input type="text" name="Pbutton_key_'+ i +'"> </div> </div><!--二级菜单--> <div style="display: none;"> <ul> <a href="#" class="js-newChild" onclick="newChild(this);">点击添加</a> </ul> </div> </div></li>';
+        var html = '<li data-id="'+ i +'"><hr><a href="javascript:" class="js-delete" onclick="deleteFirst(this)">删除</a><div> 名称：<input type="text" name="Pbutton_name_'+ i +'"> <div> 类型：<select name="Pbutton_type_'+ i +'" class="js-button-type" onchange="ButtonType(this);"> <option value="">请选择按钮类型</option> <option value="sub">菜单</option><?php foreach ($button_type_arr as $k => $v) { ?><option value="<?php echo $k; ?>"><?php echo $v; ?></option><?php } ?></select> </div> <div class="js-button-type-box"> <div><div> 链接：<input type="text" name="Pbutton_url_'+ i +'"> </div> <div> key ：<input type="text" name="Pbutton_key_'+ i +'"> </div></div> </div><!--二级菜单--> <div style="display: none;"> <ul> <a href="#" class="js-newChild" onclick="newChild(this);">点击添加</a> </ul> </div> </div></li>';
         $(_this).parent().before(html);
+        update_select();
     }
 
     /* 添加新的二级菜单 */
     function newChild(_this) {
         var num = $(_this).siblings().length;
         var i = $(_this).parent().parent().parent().parent().data("id"); /*一级菜单的个数*/
-        var html = '<li><a href="javascript:" class="js-delete" onclick="deleteChild(this)">删除</a><div> 名称：<input type="text" name="Cbutton_names_'+ i +'[]" value=""> </div> <div> 类型：<select name="Cbutton_types_'+ i +'[]" > <option value="">请选择按钮类型</option><?php foreach ($button_type_arr as $k => $v) { ?><option value="<?php echo $k; ?>"><?php echo $v; ?></option><?php } ?></select> </div> <div> 链接：<input type="text" name="Cbutton_urls_'+ i +'[]"> </div> <div> key ：<input type="text" name="Cbutton_keys_'+ i +'[]"> </div> </li>';
+        var html = '<li><a href="javascript:" class="js-delete" onclick="deleteChild(this)">删除</a><div> 名称：<input type="text" name="Cbutton_names_'+ i +'[]" value=""> </div> <div> 类型：<select onchange="ButtonType(this);" name="Cbutton_types_'+ i +'[]" > <option value="">请选择按钮类型</option><?php foreach ($button_type_arr as $k => $v) { ?><option value="<?php echo $k; ?>"><?php echo $v; ?></option><?php } ?></select> </div> <div><div> 链接：<input type="text" name="Cbutton_urls_'+ i +'[]"> </div> <div> key ：<input type="text" name="Cbutton_keys_'+ i +'[]"> </div></div> </li>';
         if (num >= 4) {
             $(".js-newChild").hide();
             // alert("最多只能存在5个二级菜单");
             // return false;
         }
         $(_this).before(html);
+        update_select();
     }
 
     /* 删除菜单 */
@@ -181,6 +191,12 @@
                 alert('提交失败');
                 console.log(data);
             }
+        });
+    }
+    /*更新select对应的div显示*/
+    function update_select(){
+        $("select").each(function(index, el) {
+            ButtonType(el);
         });
     }
 
