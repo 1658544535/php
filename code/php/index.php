489f99ba6298771a63d6b5475276ac5eae13693b
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-11-21 01:52:31 +0000

    首页二级分类更换为一级分类

--- code/php/index.php
+++ code/php/index.php
@@ -6,6 +6,11 @@ require_once('./global.php');
 $cates = apiData('productTypeNav.do');
 $cates = $cates['result'];
 
+//首页一级分类
+$classification = apiData('productCategoryApi.do');
+$classification = $classification['result'];
+
+
 //团免券
 $freeCpn = apiData('checkGroupFreeApi.do', array('userId'=>$userid));
 $freeCpn = $freeCpn['success'] ? $freeCpn['result'] : null;
