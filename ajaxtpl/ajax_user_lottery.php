58ea2311ba81dbe6437ea0623cba8dcbc781b802
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-11-24 08:55:07 +0000

    我的抽奖列表新活动类型参数

--- ajaxtpl/ajax_user_lottery.php
+++ ajaxtpl/ajax_user_lottery.php
@@ -3,8 +3,8 @@ define('HN1', true);
 require_once('../global.php');
 $page = max(1, intval($_POST['page']));
 $uId  = CheckDatas( 'uid', '' );
-
-$userLottery = apiData('getDrawApi.do', array('pageNo'=>$page,'userId'=>$uId));
+$Type  = CheckDatas( 'type', '' );
+$userLottery = apiData('getDrawApi.do', array('pageNo'=>$page,'userId'=>$uId,'type'=>$Type));
 if(!empty($userLottery['result']))
 {
     echo	ajaxJson( 1,'获取成功',$userLottery['result'],$page);
