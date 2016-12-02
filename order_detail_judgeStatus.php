a09b927228517a500106e823e63c5e10153882f0
dushaojin <dushaojin@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-02 09:13:52 +0000

    订单页详情修改

---
+++ order_detail_judgeStatus.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * 订单详情字段判断
+ * Created by PhpStorm.
+ * User: Jasper
+ * Date: 2016/12/2 0002
+ * Time: 14:17
+ */
+
+if($OrderDetail['result']['source'] != 5 && $OrderDetail['result']['source'] != 7) {
+    if ($OrderDetail['result']['isSuccess'] ==2) {
+        $statusTitle = '拼团失败';
+        $statusState = '未成团，退款中';
+        $statusTips  = '未成团，退款中';
+        $statusImg   = 8;
+        if ($OrderDetail['result']['refPriStatus'] ==2) {
+            $statusState = '未成团，退款成功';
+            $statusTips  = '未成团，退款成功';
+            $statusImg   = 7;
+        };
+    }
+
+    switch ($OrderDetail['result']['orderStatus']) {
+        case 1:
+            if ($OrderDetail['result']['isCancel'] == 0) {
+                $statusTitle = '确认订单';
+                $statusState = '等待买家付款';
+                $statusTips  = '待支付';
+                $statusImg   = 2;
+            }
+            if ($OrderDetail['result']['isCancel'] == 1) {
+                $statusTitle = '交易已取消';
+                $statusState = '交易已取消';
+                $statusTips  = '交易已取消';
+                $statusImg   = 5;
+            }
+            break;
+        case 2:
+            if ($OrderDetail['result']['isSuccess'] ==1) {
+                $statusTitle = '拼团成功';
+                $statusState = '拼团成功，等待卖家发货！';
+                $statusTips  = '已成团，待发货';
+                $statusImg   = 1;
+            }
+            if ($OrderDetail['result']['isSuccess'] ==0) {
+                $statusTitle = '拼团中';
+                $statusState = '拼团还未成功，赶快召唤小伙伴！';
+                $statusTips  = '拼团中';
+                $statusImg   = 4;
+            }
+            break;
+        case 3:
+            $statusTitle = '待收货';
+            $statusState = '卖家已发货';
+            $statusTips  = '待收货';
+            $statusImg   = 4;
+            break;
+        case 4:
+            $statusTitle = '已签收';
+            $statusState = '交易成功！';
+            $statusTips  = '已签收';
+            $statusImg   = 6;
+            break;
+    }
+}
+
+if ($OrderDetail['result']['source'] == 7 || $OrderDetail['result']['source'] == 5 ) {
+
+    $titleArr = array(
+        1 => '确认订单',
+        2 => '拼团中',
+        3 => '拼团失败',
+        4 => '拼团失败',
+        5 => '交易已取消',
+        6 => '未中奖',
+        7 => '未中奖',
+        9 => '已签收',
+        10=> '拼团成功',
+        11=> '待收货',
+        12=> '待开奖'
+    );
+
+    $stateArr = array(
+        1 => '等待买家付款',
+        2 => '拼团还未成功，赶快召唤小伙伴！',
+        3 => '未成团，退款中',
+        4 => '未成团，已退款',
+        5 => '交易已取消',
+        6 => '未中奖，返款中',
+        7 => '未中奖，已返款',
+        9 => '交易成功！',
+        10=> '拼团成功，等待卖家发货！',
+        11=> '卖家已发货',
+        12=> '已成团，待开奖'
+    );
+
+    $tipsArr = array(
+        1 => '待支付',
+        2 => '拼团中',
+        3 => '未成团，退款中',
+        4 => '未成团，已退款',
+        5 => '交易已取消',
+        6 => '未中奖，返款中',
+        7 => '未中奖，已返款',
+        9 => '已签收',
+        10=> '已中奖，待发货',
+        11=> '待收货',
+        12=> '已成团，待开奖'
+    );
+
+    $statusTitle = $titleArr[$OrderDetail['result']['orderStatus']];
+    $statusState = $stateArr[$OrderDetail['result']['orderStatus']];
+    $statusTips  = $tipsArr[$OrderDetail['result']['orderStatus']];
+    $statusImg   = getStatusImg($OrderDetail['result']['orderStatus']);
+
+    if ($OrderDetail['result']['source'] ==7 && $OrderDetail['result']['orderStatus'] ==3) {
+        $statusState = '未成团，已退款';
+        $statusTips  = '未成团，已退款';
+    };
+    if ($OrderDetail['result']['source'] ==7 && $OrderDetail['result']['orderStatus'] ==6){
+        $statusState = '未中奖，已返款';
+        $statusTips  = '未中奖，已返款';
+    };
+
+}
+
+/*
+ * 根据状态返回相应的状态北京图片
+ * 用于免费抽奖
+ * @param $status 订单状态 $OrderDetail['result']['orderStatus']
+ * @return int 状态图片标识
+ */
+function getStatusImg($status = ''){
+    if (empty($status)) return false;
+    $imgArr = array(
+        1  => 2,
+        2  => 4,
+        3  => 8,
+        4  => 7,
+        5  => 5,
+        6  => 7,
+        7  => 7,
+        9  => 6,
+        10 => 1,
+        11 => 3,
+        12 => 4,
+    );
+    return $imgArr[$status];
+}
+
+?>
---
+++ tpl/order_detail_web_new_20161202.php
@@ -0,0 +1,187 @@
+<?php include_once('header_notice_web.php');?>
+<?php include_once('wxshare_web.php');?>
+<body>
+    <div class="page-group" id="page-orderCofirm">
+        <div id="page-nav-bar" class="page page-current">
+            <header class="bar bar-nav">
+                <a class="button button-link button-nav pull-left back" href="javascript:history.back(-1);">
+                    <span class="icon icon-back"></span>
+                </a>
+                <h1 class="title"><?php echo $statusTitle;?></h1>
+            </header>
+
+            <div class="content native-scroll" style="bottom:2.75rem;">
+                <div class="oc-state">
+                    <span><?php echo $statusState;?></span>
+                    <i class="o-icon o-icon-<?php echo $statusImg;?>"></i>
+                </div>
+
+                <section class="oc-adress oc-adress-disable">
+                    <a>
+                        <div><?php echo $OrderDetail['result']['addressInfo']['consignee'];?></div>
+                        <div><?php echo $OrderDetail['result']['addressInfo']['tel'];?></div>
+                        <div><?php echo $OrderDetail['result']['addressInfo']['address'];?></div>
+                    </a>
+                </section>
+
+            <section class="freeList proTips-2 oc-pro">
+
+                <h3 class="title1">拼团商品<span class="tips"><?php echo $statusTips;?></span></h3>
+
+                    <ul class="list-container">
+                        <li><a href="/groupon.php?act=guess&id=<?php echo $OrderDetail['result']['activityId'];?>&pid=<?php echo $OrderDetail['result']['productInfo']['productId'];?>">
+                            <div class="img"><img src="<?php echo $OrderDetail['result']['productInfo']['productImage'];?>"></div>
+                            <div class="info">
+                                <div class="name"><?php echo $OrderDetail['result']['productInfo']['productName'];?></div>
+                               <div class="sub">共<?php echo $OrderDetail['result']['productInfo']['number'];?>件商品&nbsp;合计：<font class="themeColor">￥<span class="price"><?php echo $OrderDetail['result']['productInfo']['allPrice'];?></span></font>（免运费）</div>
+                            </div>
+                        </a></li>
+                    </ul>
+                <?php if($OrderDetail['result']['source'] != 5 && $OrderDetail['result']['source'] != 7){?>
+                    <div class="option">
+                        <?php if($OrderDetail['result']['orderStatus'] !=1){?>
+                            <?php if ($OrderDetail['result']['orderStatus'] ==3) { //已发货状态 ?>
+                                <?php if($OrderDetail['result']['refundStatus'] ==0 ){ //未申请过售后 ?>
+                                    <a href="aftersale.php?act=apply&oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'];?>">申请退款</a>
+                                <?php } elseif ($OrderDetail['result']['refundStatus'] == 1 || $OrderDetail['result']['refundStatus'] == 2 || $OrderDetail['result']['refundStatus'] == 3) { //申请过售后 ?>
+                                    <a class="txt">售后申请中...</a>
+                                <?php } else { ?>
+                                    <a href="groupon_join.php?aid=<?php echo $OrderDetail['result']['attendId']; ?>" class="gray">查看团详情</a>
+                                <?php } ?>
+                            <?php } ?>
+                        <?php }?>
+                    </div>
+                <?php } else { ?>
+                    <div class="option">
+                        <?php if($OrderDetail['result']['orderStatus'] != 2 || $OrderDetail['result']['orderStatus'] != 1) { //待支付 拼团中,不显示中奖记录 ?>
+                            <a href="lottery_new.php?act=winning&aid=<?php echo $OrderDetail['result']['activityId']; ?>&attId=<?php echo $OrderDetail['result']['attendId']; ?>&type=<?php if($OrderDetail['result']['source']==5){?>5<?php }elseif($OrderDetail['result']['source']==7){?>7<?php }?>">查看中奖记录</a>
+                        <?php } ?>
+                        <a href="groupon_join.php?aid=<?php echo $OrderDetail['result']['attendId']; ?>">查看团详情</a>
+                        <?php if ($OrderDetail['result']['source']==5) { ?>
+                            <?php if ($OrderDetail['result']['orderStatus'] ==11) { ?>
+                                <a href="aftersale.php?act=apply&oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'];?>">申请退款</a>
+                            <?php } ?>
+                            <?php if ($OrderDetail['result']['refundStatus'] ==1 || $OrderDetail['result']['refundStatus'] ==2 || $OrderDetail['result']['refundStatus'] ==3) { ?>
+                                <a class="txt">售后申请中...</a>
+                            <?php } ?>
+                        <?php } ?>
+                    </div>
+                <?php }?>
+            </section>
+
+                <section class="oc-info">
+                    <div>订单编号：<?php echo $OrderDetail['result']['orderInfo']['orderNo'];?></div>
+
+                    <?php if($OrderDetail['result']['orderInfo']['paymethod'] ==1){?>
+                    <div>支付方式：支付宝</div>
+                    <?php }else if($OrderDetail['result']['orderInfo']['paymethod'] ==2){?>
+                    <div>支付方式：微信支付</div>
+                    <?php }else if($OrderDetail['result']['orderInfo']['paymethod'] ==8){?>
+                    <div>支付方式：微信支付</div>
+                    <?php }?>
+                    <div>下单时间：<?php echo $OrderDetail['result']['orderInfo']['createTime'];?></div>
+                    <?php if($OrderDetail['result']['source'] !=5 && $OrderDetail['result']['source'] !=7 && $OrderDetail['result']['orderStatus'] ==2  && $OrderDetail['result']['isSuccess'] ==1){?>
+                    <div>成团时间：<?php echo $OrderDetail['result']['orderInfo']['groupTime'];?></div>
+                    <?php }elseif($OrderDetail['result']['source'] !=5 && $OrderDetail['result']['source'] !=7 && $OrderDetail['result']['orderStatus'] ==3){?>
+                     <div>成团时间：<?php echo $OrderDetail['result']['orderInfo']['groupTime'];?></div>
+                    <?php }elseif((($OrderDetail['result']['source'] ==5) || ($OrderDetail['result']['source'] ==7)) && (($OrderDetail['result']['orderStatus'] ==6) || ($OrderDetail['result']['orderStatus'] ==7) || ($OrderDetail['result']['orderStatus'] ==9) || ($OrderDetail['result']['orderStatus'] ==10) || ($OrderDetail['result']['orderStatus'] ==11))){?>
+                    <div>成团时间：<?php echo $OrderDetail['result']['orderInfo']['groupTime'];?></div>
+                    <?php }elseif(($OrderDetail['result']['orderStatus'] ==4) || ($OrderDetail['result']['orderStatus'] ==9)){?>
+                     <div>成团时间：<?php echo $OrderDetail['result']['orderInfo']['groupTime'];?></div>
+                     <div>发货时间：<?php echo $OrderDetail['result']['orderInfo']['sendTime'];?></div>
+                     <div>成交时间：<?php echo $OrderDetail['result']['orderInfo']['confirmTime'];?></div>
+                     <div>快递方式：<?php echo $OrderDetail['result']['orderInfo']['logisticsName'];?></div>
+                     <div>运单编号：<?php echo $OrderDetail['result']['orderInfo']['logisticsNo'];?></div>
+                    <?php }?>
+                </section>
+          <?php if($OrderDetail['result']['orderStatus'] ==1 && $OrderDetail['result']['isCancel'] ==0){?>
+                <section class="oc-pay">
+                    <ul class="list">
+                        <li>
+                            <input type="radio" name="payWay" checked />
+                            <img src="images/pay-wx.png" />
+                            <p>微信支付</p>
+                            <span class="label">推荐</span>
+                        </li>
+                        <!--  <li>
+                            <input type="radio" name="payWay" />
+                            <img src="images/pay-zfb.png" />
+                            <p>支付宝支付</p>
+                        </li>
+                        <li>
+                            <input type="radio" name="payWay" />
+                            <img src="images/pay-qq.png" />
+                            <p>QQ钱包</p>
+                        </li>-->
+                    </ul>
+                </section>
+             <?php }?>
+
+
+            </div>
+          <?php if($OrderDetail['result']['source'] !=5 && $OrderDetail['result']['source'] !=7){?>
+            <div class="oc-footer2">
+            	<?php if($OrderDetail['result']['orderStatus'] ==1 && $OrderDetail['result']['isCancel'] ==0){?>
+                    <!-- <a class="one" id="orderCancel"> href="order_detail.php?act=cancel&oid=<?php echo $OrderDetail['result']['orderId'] ;?>">取消订单</a> -->
+                    <a class="btn gray" id="orderCancel">取消订单</a>
+                    <a class="btn" href="/wxpay/pay.php?oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'] ;?>">去支付</a>
+              <?php }else if($OrderDetail['result']['orderStatus'] ==3){?>
+                    <a href="logistics.php?oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'];?>" class="btn gray">查看物流</a>
+                   <?php if($OrderDetail['result']['refundStatus'] ==0 || $OrderDetail['result']['refundStatus'] ==6 || $OrderDetail['result']['refundStatus'] ==5){?>
+                    <a id="check" class="btn" data-id="<?php echo $OrderDetail['result']['orderInfo']['orderId'] ;?>" data-status="<?php echo $OrderDetail['result']['orderStatus']  ;?>">确认收货</a>
+                   <?php }else if($OrderDetail['result']['refundStatus'] ==1 || $OrderDetail['result']['refundStatus'] ==2 || $OrderDetail['result']['refundStatus'] ==3){?>
+                    <a class="txt">售后申请中...</a>
+                   <?php }?>
+             <?php }else if($OrderDetail['result']['orderStatus'] ==4){?>
+                     <a href="logistics.php?oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'];?>" class="btn">查看物流</a>
+             <?php }?>
+            </div>
+		<?php }else{?>
+			<div class="oc-footer2">
+	            	<?php if($OrderDetail['result']['orderStatus'] ==1 && $OrderDetail['result']['isCancel'] ==0){?>
+	                    <a class="btn gray" id="orderCancel">取消订单</a>
+	                    <a class="btn" href="/wxpay/pay.php?oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'] ;?>">去支付</a>
+	              <?php }else if($OrderDetail['result']['orderStatus'] ==11){?>
+	                    <a href="logistics.php?oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'];?>" class="btn gray">查看物流</a>
+	                   <?php if($OrderDetail['result']['refundStatus'] ==0 || $OrderDetail['result']['refundStatus'] ==6 || $OrderDetail['result']['refundStatus'] ==5){?>
+	                    <a id="check" class="btn" data-id="<?php echo $OrderDetail['result']['orderInfo']['orderId'] ;?>" data-status="<?php echo $OrderDetail['result']['orderStatus']  ;?>">确认收货</a>
+	                   <?php }else if($OrderDetail['result']['refundStatus'] ==1 || $OrderDetail['result']['refundStatus'] ==2 || $OrderDetail['result']['refundStatus'] ==3){?>
+	                    <a class="txt">售后申请中...</a>
+	                   <?php }?>
+	             <?php }else if($OrderDetail['result']['orderStatus'] ==9){?>
+	                     <a href="logistics.php?oid=<?php echo $OrderDetail['result']['orderInfo']['orderId'];?>" class="btn">查看物流</a>
+	             <?php }?>
+	            </div>
+		<?php }?>
+
+            <script>
+                $(document).on("pageInit", "#page-orderCofirm", function(e, pageId, page) {
+                    $("#orderCancel").on("click", function(){
+                    	var _this = $(this);
+	                	$.confirm("是否取消订单？", function(){
+	                        $.post("order_detail.php",{act: "cancel", oid:"<?php echo $OrderDetail['result']['orderInfo']['orderId'] ;?>"},function(req){
+	                        	req =  eval("(" + req + ")");
+	                            $.toast(req.data.data.error_msg);
+	                            history.back(-1);
+	                        },"JSON");
+	                	})
+                    });
+                });
+                $(document).on("click", "#check", function(){
+	                var _this = $(this);
+                	$.confirm("是否确定收货？", function(){
+	                    $.post("order_detail.php",{act: "edit", oid:_this.attr("data-id"), status:_this.attr("data-status")},function(req){
+	                    	req =  eval("(" + req + ")");
+	                        $.toast(req.data.data.error_msg);
+							history.back(-1);
+	                    },"JSON");
+                	});
+                });
+            </script>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
