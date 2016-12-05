b987a3dfb1dbeeb5d7ebc5590c4f99615bfc4007
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-05 08:07:01 +0000

    去除已废代码

--- tpl/header_notice_web.php
+++ tpl/header_notice_web.php
@@ -19,75 +19,43 @@
     <script type='text/javascript' src='js/swiper/swiper.min.js' charset='utf-8'></script>
     <script type='text/javascript' src='js/app.min.js?v=<?php echo SOURCE_VERSOIN;?>' charset='utf-8'></script>
     <script>
-	    $.ajax({  
-		        url: "msg.php",
-		        data: {"act":"plan2"},  
-		        dataType: "json",  
-		        success: function (data) {
-		            var data = data["data"]["data"],
-		                degree = 1,
-		                pushTxt = '';
-
-                    //打乱数组数据
-                    var arrLen = data.length,
-                        keyArr = [];
-                    for(var i=0; i<arrLen; i++){
-                        keyArr.push(i);
-                    };
-                    keyArr.sort(function(){ return 0.5 - Math.random()});
-                    var newDataArr = [];
-                    for(var j=0; j<keyArr.length; j++){
-                        newDataArr.push(data[keyArr[j]]);
-                    };
-                    data = newDataArr;
-                    
-		            fn_timer();
-		            var timer = setInterval(fn_timer, 6000);
-		            function fn_timer(){
-		                if(degree<=data.length){
-		                    pushTxt = '<a href="groupon_join.php?aid='+data[degree-1]["attendId"]+'" class="message-push"><img src="'+data[degree-1]["icon"]+'" class="img" />新订单来自'+data[degree-1]["address"]+'的【'+data[degree-1]["userName"]+'】一起拼！</a>';
-		                    $(".page-group").append(pushTxt);
-		                    setTimeout(function(){
-		                        $(".message-push").remove();
-		                    }, 4000);
-		                    degree++;
-		                }else{
-		                    clearInterval(timer);
-		                }
-		            }
-		        }  
-		    });
-
-
-
-//         $(function(){
-//             (function longPolling() {  
-//                 $.ajax({  
-//                     url: "msg.php",
-//                     data: {"act":"plan1","timed": new Date().getTime()},  
-//                     dataType: "json",  
-//                     timeout: 5000,//5秒超时，可自定义设置  
-//                     error: function (XMLHttpRequest, textStatus, errorThrown) {  
-//                    	 if (textStatus == "timeout") { // 请求超时  
-// 	                             longPolling(); // 递归调用  
-// 	                         }
-//                          },  
-//                     success: function (data, textStatus) {  
-//                             var pushTxt = '<a href="1" class="message-push"><img src="http://pin.taozhuma.com/upfiles/focusbanner/20161004093329310841.jpg" class="img" />最新订单来自 【地址】 的 【用户昵称】，1秒前</a>';
-//                             if($(".message-push".length>0)){
-//                                 $(".message-push").remove();
-//                             }
-//                             $(".page-group").append(pushTxt);
-//                             setTimeout(function(){
-//                                 $(".message-push").remove();
-//                             }, 8000);
-//                             if (textStatus == "success") { // 请求成功  
-//                                 longPolling();  
-//                             }    
-                         
-//                     }  
-//                 });  
-//             })();
-//         });
+//	    $.ajax({  
+//		        url: "msg.php",
+//		        data: {"act":"plan2"},  
+//		        dataType: "json",  
+//		        success: function (data) {
+//		            var data = data["data"]["data"],
+//		                degree = 1,
+//		                pushTxt = '';
+//
+//                    //打乱数组数据
+//                    var arrLen = data.length,
+//                        keyArr = [];
+//                    for(var i=0; i<arrLen; i++){
+//                        keyArr.push(i);
+//                    };
+//                    keyArr.sort(function(){ return 0.5 - Math.random()});
+//                    var newDataArr = [];
+//                    for(var j=0; j<keyArr.length; j++){
+//                        newDataArr.push(data[keyArr[j]]);
+//                    };
+//                    data = newDataArr;
+//                    
+//		            fn_timer();
+//		            var timer = setInterval(fn_timer, 6000);
+//		            function fn_timer(){
+//		                if(degree<=data.length){
+//		                    pushTxt = '<a href="groupon_join.php?aid='+data[degree-1]["attendId"]+'" class="message-push"><img src="'+data[degree-1]["icon"]+'" class="img" />新订单来自'+data[degree-1]["address"]+'的【'+data[degree-1]["userName"]+'】一起拼！</a>';
+//		                    $(".page-group").append(pushTxt);
+//		                    setTimeout(function(){
+//		                        $(".message-push").remove();
+//		                    }, 4000);
+//		                    degree++;
+//		                }else{
+//		                    clearInterval(timer);
+//		                }
+//		            }
+//		        }  
+//		    });
         </script>
 </head>
