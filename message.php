685858eab456ec487d37d042793aa490dd091408
dushaojin <dushaojin@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-12 01:17:30 +0000

    消息详情生成链接修改

--- message.php
+++ message.php
@@ -8,16 +8,23 @@ require_once('./global.php');
 $act = CheckDatas('act', '');
 
 switch ($act) {
-    case 'scroll': //无限滚动
+    /*---------------------------------------------------------------------------
+        --  瀑布流信息无限加载
+     ---------------------------------------------------------------------------*/
+    case 'scroll':
         $messageType = CheckDatas('type', 0);
-
         include_once ('./tpl/message/msg_scroll_tpl.php');
         break;
-    case 'detail': //消息详情
-
+    /*---------------------------------------------------------------------------
+        --  消息详情
+     ---------------------------------------------------------------------------*/
+    case 'detail':
         include_once ('./tpl/message/msg_detail_tpl.php');
         break;
-    default: //消息列表
+    /*---------------------------------------------------------------------------
+        --  消息列表
+     ---------------------------------------------------------------------------*/
+    default:
         $getMessageList = apiData('myNoticeApi.do', array('userId' => $userid));
         if ($getMessageList['success'] != false) {
             $MessageList = $getMessageList['result'];
