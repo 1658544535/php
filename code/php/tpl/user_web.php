d7eb3801eb20768b41df9a57aaba7df335af499b
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-13 12:55:46 +0000

    修改未付款订单字体大小

--- code/php/tpl/user_web.php
+++ code/php/tpl/user_web.php
@@ -25,9 +25,9 @@
                     <?php }else{?>
                     <div class="name"><?php echo $_wxUserInfo['nickname']; ?></div>
                     <?php }?>
-                    <div class="orderTips">
+                    <div class="orderTips" style="font-size: 14px">
                     <?php if($info['waitPayNum'] > 0){ ?>
-                        <a href="user_orders.php?type=1">还有<span class="themeColor" style="font-size: 14px" ><?php echo $info['waitPayNum'];?>个订单</span>未付款 ></a>
+                        <a href="user_orders.php?type=1">还有<span class="themeColor"><?php echo $info['waitPayNum'];?>个订单</span>未付款 ></a>
                     <?php } ?>
                     </div>
                     <ul class="orderTab">
