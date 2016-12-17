72b51fad9bba5d1505117968e106756a79de7f67
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-17 06:37:55 +0000

    增加落地页

---
+++ code/php/tpl/zhuan_web.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=0" name="viewport">
+        <title>落地页</title>
+        <style type="text/css">
+			html,body,.wrap{width: 100%;height: 100%;margin: 0;padding: 0;}
+			.wrap{position:relative;max-width:640px;margin:0 auto;background: #ed464f;}
+            .img-header{position: absolute;left: 0;top: 0;width: 100%;z-index: 3;}
+            .img-adorn{position: absolute;left: 0;bottom: 0;width: 100%;height:100%;background:url(images/ldy/adorn.png) no-repeat center center;background-size:cover;z-index: 4;}
+            .img-footer{position: absolute;left: 0;bottom: 0;width: 100%;z-index: 2;}
+            .fullImg{display: block;width: 100%;}
+            .main{position: absolute;top: 14%;left: 0;width: 100%;z-index:5;}
+            .img-btn{display:block;width: 35.8293%;padding-bottom: 11.6747%;margin:3% auto 0;background: url(images/ldy/btn.png) no-repeat 0 0;background-size: 100% 100%;font-size: 0;line-height: 0;}
+        </style>
+    </head>
+    <body>
+        
+        <div class="wrap">
+            <div class="img-header"><img class="fullImg" src="images/ldy/header.png" /></div>
+            <div class="img-adorn"></div>
+            <div class="main">
+                <div class="img-title"><img class="fullImg" src="images/ldy/txt.png" /></div>
+                <a href="http://a.app.qq.com/o/simple.jsp?pkgname=com.qunyu.taoduoduo" class="img-btn">速来抢</a>
+            </div>
+            <div class="img-footer"><img class="fullImg" src="images/ldy/footer.jpg" /></div>
+        </div>
+
+    </body>
+</html>
