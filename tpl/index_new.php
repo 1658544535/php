1b8727c451a462614df433da2632678a4b9d2fd3
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-19 12:00:56 +0000

    更换分享logo

--- tpl/index_new.php
+++ tpl/index_new.php
@@ -2,7 +2,7 @@
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script type="text/javascript" src="/js/wxshare.js"></script>
 <script type="text/javascript">
-var imgUrl = "<?php echo $site;?>images/wxLOGO.png";
+var imgUrl = "<?php echo $site;?>images/200.jpg";
 var link = "<?php echo $site;?>";
 var title ="拼得好";
 var desc = "1毛夺好礼，拼享新玩法";
