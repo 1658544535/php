c3129e2f662c1b0263d91336950068f48af579e4
xujiakai <xujiakai@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-21 02:58:09 +0000

    sku优化

--- code/php/tpl/groupon_join_web.php
+++ code/php/tpl/groupon_join_web.php
@@ -362,6 +362,11 @@ wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam[
 										break;
 								}
 							}
+							$(".sku-item").each(function(index, el) {
+								if($(el).find(".list").html() == ''){
+									$(el).remove();
+								}
+							});
 
 							//将可选的sku存入一个全局变量
 							skuData = data["validSKu"];
@@ -410,28 +415,56 @@ wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam[
 								}
 
 								if(!!skuFormat){
-									// $("#sku-format .list a.active").siblings('a').addClass("disable");
-									$("#sku-color .list a").not(".active").addClass("disable");
-									for(var item in skuData){
-										if(skuData[item]["skuFormat"] == skuFormat){
-											$("#sku-color .list a").each(function(index, el) {
-												if($(el).html() == skuData[item]["skuColor"]){
-													$(el).removeClass("disable");
-												}
-											});
+									// 只有规格时
+									if(data["skuList"].length==1 && data["skuList"][0]["skuType"] == 2){
+										var url = $(".popup-sku").attr("data-href"),
+											skuId = '';
+										var skuImg = '';
+										for(var item in skuData){
+											if(skuData[item]["skuFormat"] == skuFormat){
+												skuId = skuData[item]["id"];
+												skuImg = skuData[item]["skuImg"];
+											}
+										}
+										setSkuInfo(skuId, skuImg);
+										clickBuy = true;
+									}else{
+										$("#sku-color .list a").not(".active").addClass("disable");
+										for(var item in skuData){
+											if(skuData[item]["skuFormat"] == skuFormat){
+												$("#sku-color .list a").each(function(index, el) {
+													if($(el).html() == skuData[item]["skuColor"]){
+														$(el).removeClass("disable");
+													}
+												});
+											}
 										}
 									}
 								}
 								if(!!skuColor){
-									// $("#sku-color .list a.active").siblings('a').addClass("disable");
-									$("#sku-format .list a").not(".active").addClass("disable");
-									for(var item in skuData){
-										if(skuData[item]["skuColor"] == skuColor){
-											$("#sku-format .list a").each(function(index, el) {
-												if($(el).html() == skuData[item]["skuFormat"]){
-													$(el).removeClass("disable");
-												}
-											});
+									// 只有颜色时
+									if(data["skuList"].length==1 && data["skuList"][0]["skuType"] == 1){
+										var url = $(".popup-sku").attr("data-href"),
+											skuId = '';
+										var skuImg = '';
+										for(var item in skuData){
+											if(skuData[item]["skuColor"] == skuColor){
+												skuId = skuData[item]["id"];
+												skuImg = skuData[item]["skuImg"];
+											}
+										}
+										setSkuInfo(skuId, skuImg);
+										clickBuy = true;
+									}else{
+										$("#sku-format .list a").not(".active").addClass("disable");
+										for(var item in skuData){
+											if(skuData[item]["skuColor"] == skuColor){
+												$("#sku-format .list a").each(function(index, el) {
+													if($(el).html() == skuData[item]["skuFormat"]){
+														$(el).removeClass("disable");
+													}
+												});
+											}
 										}
 									}
 								}
@@ -450,11 +483,7 @@ wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam[
 											skuImg = skuData[item]["skuImg"];
 										}
 									}
-									_genUrl({"skuid":skuId});
-									if(skuImg !="" && skuImg != null){
-										$(".popup-sku .info .img img").attr("src", skuImg);
-									}
-									$("#buy").removeClass("gray");
+									setSkuInfo(skuId, skuImg);
 									clickBuy = true;
 								}else if(!skuFormat && !skuColor){
 									$("#sku-color .list a, #sku-format .list a").removeClass("disable");
@@ -468,6 +497,14 @@ wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam[
 						$.popup(".popup-sku");      //弹出弹窗
 						$("#buy-num").val(1);
 						$.hideIndicator();          //关闭加载指示器
+					}
+
+					function setSkuInfo(skuId, skuImg){
+						_genUrl({"skuid":skuId});
+						if(skuImg !="" && skuImg != null){
+							$(".popup-sku .info .img img").attr("src", skuImg);
+						}
+						$("#buy").removeClass("gray");
 					}
 
 					function _genUrl(_json){
