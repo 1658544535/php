98a868e8d398aace3af57364d229c6524d8cbb1a
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-29 02:13:21 +0000

    微信库增加标签管理方法

--- code/php/includes/lib/Weixin.class.php
+++ code/php/includes/lib/Weixin.class.php
@@ -12,6 +12,15 @@ class Weixin extends Wechat{
     private $logDir;
     private $oauthAccessTokenVar = 'oauth_access_token';
 
+    const TAG_GET_URL='/tags/get?';
+	const TAG_CREATE_URL='/tags/create?';
+	const TAG_UPDATE_URL='/tags/update?';
+    const TAG_DELETE_URL='/tags/delete?';
+    const TAG_USER_URL='/user/tag/get?';
+    const TAG_MEMBER_BATCHTAG_URL = '/tags/members/batchtagging?';
+    const TAG_MEMBER_BATCHUNTAG_URL = '/tags/members/batchuntagging?';
+    const TAG_MEMBER_IDLIST_URL = '/tags/getidlist?';
+
     public function __construct($options){
         parent::__construct($options);
         $this->cacheDir = WX_CACHE_DIR.'/';
@@ -97,5 +106,208 @@ class Weixin extends Wechat{
         }else{
             return true;
         }
+    }
+
+    /**
+	 * 获取标签列表
+     *
+	 * @return boolean|array
+	 */
+	public function getTag(){
+		if (!$this->access_token && !$this->checkAuth()) return false;
+		$result = $this->http_get(self::API_URL_PREFIX.self::TAG_GET_URL.'access_token='.$this->access_token);
+		if ($result)
+		{
+			$json = json_decode($result,true);
+			if (isset($json['errcode'])) {
+				$this->errCode = $json['errcode'];
+				$this->errMsg = $json['errmsg'];
+				return false;
+			}
+			return $json;
+		}
+		return false;
+	}
+
+	/**
+	 * 新增自定标签
+     *
+	 * @param string $name 标签名称
+	 * @return boolean|array
+	 */
+	public function createTag($name){
+		if (!$this->access_token && !$this->checkAuth()) return false;
+		$data = array(
+			'tag'=>array('name'=>$name)
+		);
+		$result = $this->http_post(self::API_URL_PREFIX.self::TAG_CREATE_URL.'access_token='.$this->access_token,self::json_encode($data));
+		if ($result)
+		{
+			$json = json_decode($result,true);
+			if (!$json || !empty($json['errcode'])) {
+				$this->errCode = $json['errcode'];
+				$this->errMsg = $json['errmsg'];
+				return false;
+			}
+			return $json;
+		}
+		return false;
+	}
+
+	/**
+	 * 更改标签名称
+     *
+	 * @param int $tagid 标签id
+	 * @param string $name 标签名称
+	 * @return boolean
+	 */
+	public function updateTag($tagid,$name){
+		if (!$this->access_token && !$this->checkAuth()) return false;
+		$data = array(
+			'tag'=>array('id'=>$tagid,'name'=>$name)
+		);
+		$result = $this->http_post(self::API_URL_PREFIX.self::TAG_UPDATE_URL.'access_token='.$this->access_token,self::json_encode($data));
+		if ($result)
+		{
+			$json = json_decode($result,true);
+			if (!$json || !empty($json['errcode'])) {
+				$this->errCode = $json['errcode'];
+				$this->errMsg = $json['errmsg'];
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+    /**
+     * 删除标签
+     *
+     * @return boolean
+     */
+    public function deleteTag($tagid){
+        if (!$this->access_token && !$this->checkAuth()) return false;
+        $data = array(
+			'tag'=>array('id'=>$tagid,'name'=>$name)
+		);
+        $result = $this->http_post(self::API_URL_PREFIX.self::TAG_DELETE_URL.'access_token='.$this->access_token,self::json_encode($data));
+		if ($result)
+		{
+			$json = json_decode($result,true);
+			if (!$json || !empty($json['errcode'])) {
+				$this->errCode = $json['errcode'];
+				$this->errMsg = $json['errmsg'];
+				return false;
+			}
+			return true;
+		}
+		return false;
+    }
+
+    /**
+	 * 获取标签下的粉丝
+     *
+     * @param integer $tagid 标签id
+	 * @param string $openid
+	 * @return boolean|array
+	 */
+	public function getUserTag($tagid, $openid=''){
+	    if (!$this->access_token && !$this->checkAuth()) return false;
+	    $data = array(
+            'tagid' => $tagid,
+	        'next_openid'=>$openid
+	    );
+	    $result = $this->http_post(self::API_URL_PREFIX.self::TAG_USER_URL.'access_token='.$this->access_token,self::json_encode($data));
+	    if ($result)
+	    {
+	        $json = json_decode($result,true);
+	        if (!$json || !empty($json['errcode'])) {
+	            $this->errCode = $json['errcode'];
+	            $this->errMsg = $json['errmsg'];
+	            return false;
+	        }
+            return $json;
+	    }
+	    return false;
+	}
+
+    /**
+     * 批量给用户打标签
+     *
+     * @param integer $tagid 标签id
+     * @param array $openids 用户openid
+     * @return boolean
+     */
+    public function memberBatchTag($tagid, $openids){
+        if (!$this->access_token && !$this->checkAuth()) return false;
+        $data = array(
+            'tagid' => $tagid,
+	        'openid_list' => $openids
+	    );
+        $result = $this->http_post(self::API_URL_PREFIX.self::TAG_MEMBER_BATCHTAG_URL.'access_token='.$this->access_token,self::json_encode($data));
+        if ($result)
+	    {
+	        $json = json_decode($result,true);
+	        if (!$json || !empty($json['errcode'])) {
+	            $this->errCode = $json['errcode'];
+	            $this->errMsg = $json['errmsg'];
+	            return false;
+	        }
+            return true;
+	    }
+	    return false;
+    }
+
+    /**
+     * 批量给用户取消标签
+     *
+     * @param integer $tagid 标签id
+     * @param array $openids 用户openid
+     * @return boolean
+     */
+    public function memberBatchUnTag($tagid, $openids){
+        if (!$this->access_token && !$this->checkAuth()) return false;
+        $data = array(
+            'tagid' => $tagid,
+	        'openid_list' => $openids
+	    );
+        $result = $this->http_post(self::API_URL_PREFIX.self::TAG_MEMBER_BATCHUNTAG_URL.'access_token='.$this->access_token,self::json_encode($data));
+        if ($result)
+	    {
+	        $json = json_decode($result,true);
+	        if (!$json || !empty($json['errcode'])) {
+	            $this->errCode = $json['errcode'];
+	            $this->errMsg = $json['errmsg'];
+	            return false;
+	        }
+            return true;
+	    }
+	    return false;
+    }
+
+    /**
+     * 获取用户的标签
+     *
+     * @param string $openid 用户openid
+     * @return array
+     */
+    public function memberTagidList($openid){
+        if (!$this->access_token && !$this->checkAuth()) return false;
+        $data = array(
+            'openid' => $openid
+	    );
+        $result = $this->http_post(self::API_URL_PREFIX.self::TAG_MEMBER_IDLIST_URL.'access_token='.$this->access_token,self::json_encode($data));
+        if ($result)
+	    {
+	        $json = json_decode($result,true);
+	        if (!$json || !empty($json['errcode'])) {
+	            $this->errCode = $json['errcode'];
+	            $this->errMsg = $json['errmsg'];
+	            return false;
+	        }else{
+                if(isset($json['tagid_list'])) return $json['tagid_list'];
+            }
+	    }
+	    return false;
     }
 }
