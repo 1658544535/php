2aeb9c428d3738c0b9eebb98572182d83f3ba49b
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-30 14:02:33 +0000

    增加积分商城中转页面

---
+++ jifenshangcheng.php
@@ -0,0 +1,11 @@
+<?php
+define('HN1', true);
+require_once('./global.php');
+
+IS_USER_LOGIN();
+
+//积分商城
+$jfscUrl = 'http://wx.zhijianec.cn/app/./index.php?i=261&c=entry&pid=356&op=good&do=rank&m=junsion_poster';
+
+header('location:'.$jfscUrl);
+?>
