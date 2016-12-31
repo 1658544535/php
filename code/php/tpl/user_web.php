858ed5faf810228f0b453d8f1f92453e9512d0a5
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2016-12-31 07:50:47 +0000

    隐藏拼得客团免链接入口

--- code/php/tpl/user_web.php
+++ code/php/tpl/user_web.php
@@ -80,7 +80,7 @@
                     <ul>
                         <li><a href="/pindeke.php?act=pdkInfo&uid=<?php echo $userid;?>"><i class="u-p-1"></i>我的信息</a></li>
                         <li><a href="/pindeke.php?act=wallet&uid=<?php echo $userid;?>"><i class="u-p-2"></i>我的钱包</a></li>
-                        <li><a href="/pindeke.php?act=QRcode&uid=<?php echo $userid;?>"><i class="u-p-3"></i>团免链接</a></li>
+                        <!--  <li><a href="/pindeke.php?act=QRcode&uid=<?php echo $userid;?>"><i class="u-p-3"></i>团免链接</a></li>-->
                         <li><a href="/pindeke.php?act=mission&uid=<?php echo $userid;?>"><i class="u-p-4"></i>任务清单</a></li>
                     </ul>
                 </section>
