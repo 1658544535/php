72d693f6a9daff947d1273ae8969f26225135cef
xujiakai <xujiakai@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2017-01-03 13:19:25 +0000

    任务清单搜索

--- tpl/pdk_mission_list_web.php
+++ tpl/pdk_mission_list_web.php
@@ -8,105 +8,12 @@ var title ="拼得好";
 var desc = "1毛夺好礼，拼享新玩法";
 wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam['timestamp'];?>, '<?php echo $wxShareParam['nonceStr'];?>', '<?php echo $wxShareParam['signature'];?>', imgUrl, link, title, desc);
 </script>
-<style>
-    .user-tab{
-        position: absolute;
-        width: 100%;
-        top: 2.2rem;
-        z-index: 11;
-    }
-    .user-tab:after{
-        content: ' ';
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 1px;
-        background: #e7e7e7;
-    }
-    .user-tab ul li.double.active-type1:after{
-        display: inline-block;
-        vertical-align: top;
-        width: 0.5rem;
-        height: 100%;
-        background: url(../images/active-down.png) no-repeat 0 center;
-        background-size: 100% auto;
-        content: ' ';
-    }
-    .user-tab ul li.double.active-type2:after{
-        display: inline-block;
-        vertical-align: top;
-        width: 0.5rem;
-        height: 100%;
-        background: url(../images/active-up.png) no-repeat 0 center;
-        background-size: 100% auto;
-        content: ' ';
-    }
-    .user-tab ul li.active a{
-        border-bottom: none;
-    }
-    .index-seckill .list-container{
-        margin-top: 2px;
-    }
-    .popup-class .main{
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        background: #fff;
-    }
-    .popup-class .main .title1{
-        margin: 0;
-        height: 2.0rem;
-        line-height: 2.0rem;
-        font-size: .75rem;
-        color: #818181;
-        font-weight: 400;
-        text-indent: 1.0rem;
-        background: #e9e9e9;
-    }
-    .popup-class .main .info{
-        margin: 1.5rem 1rem;
-        overflow: hidden;
-    }
-    .popup-class .main .info .item{
-        float: left;
-        width: 30%;
-        margin: 0 1.5%;
-    }
-    .popup-class .main .info .item input{
-        display: block;
-        width: 100%;
-        height: 1.7rem;
-        line-height: 1.8rem;
-        border: 1px solid #c8c8c8;
-        border-radius: 5px;
-        text-align: center;
-    }
-    .popup-class .main .btn{
-        overflow: hidden;
-    }
-    .popup-class .main .btn a{
-        float: left;
-        width: 50%;
-        height: 2.25rem;
-        line-height: 2.25rem;
-        font-size: .8rem;
-        color: #fff;
-        text-align: center;
-        background: #ff464e;
-    }
-    .popup-class .main .btn a.gray{
-        background: #979797;
-    }
-</style>
 
 <body>
     <div class="page-group" id="page-taskList">
-        <div id="page-nav-bar" class="page page-current">
+        <div id="page-nav-bar" class="page page-current page-taskList">
             <script>
-                // var classJson = <?php echo json_encode($classOne);?>;
-                // console.log(classJson);
+                var classJson = <?php echo json_encode($classOne);?>;
             </script>
             <header class="bar bar-nav">
                 <a class="button button-link button-nav pull-left back" href="javascript:history.back(-1);">
@@ -117,15 +24,15 @@ wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam[
                     <div class="txt"><input id="search" type="text" name="name" value="" placeholder="搜索商品" /></div>
                 </div>
             </header>
-            <!-- <section class="user-tab user-tab3">
+            <section class="user-tab user-tab3">
                 <ul>
-                    <li class="double" data-type1="2" data-type2="3"><a href="javascript:;">销量</a></li>
-                    <li class="double" data-type1="2" data-type2="3"><a href="javascript:;">价格</a></li>
+                    <li class="double" data-type1="1" data-type2="11"><a href="javascript:;">销量</a></li>
+                    <li class="double" data-type1="2" data-type2="22"><a href="javascript:;">价格</a></li>
                     <li class="out"><a href="javascript:;">筛选</a></li>
                 </ul>
-            </section> -->
+            </section>
 
-            <div class="content native-scroll">
+            <div class="content native-scroll" style="top: 4.2rem;">
 
                 <section class="index-seckill pullbox infinite-scroll infinite-scroll-bottom" data-distance="30" data-href="ajaxtpl/ajax_pdk_mission.php">
                     <ul class="list-container"></ul>
@@ -170,7 +77,7 @@ wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam[
                 </div>
                 <div class="btn">
                     <a class="gray close-popup" href="javascript:;">取消</a>
-                    <a href="javascript:;">确定</a>
+                    <a id="screen" class="close-popup" href="javascript:;">确定</a>
                 </div>
             </div>
         </div>
