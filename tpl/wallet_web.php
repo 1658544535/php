073bf083d686c604934b997b033734b491b017c9
xujiakai <xujiakai@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2017-01-05 08:00:28 +0000

    兑换钱包/兑换优惠券页面toast调整

--- tpl/wallet_web.php
+++ tpl/wallet_web.php
@@ -1,14 +1,14 @@
 <?php include_once('header_web.php');?>
 <?php include_once('wxshare_web.php');?>
 <style>
-    .modal.modal-in{
+    .modal.modal-in:not(.toast){
         width: 90.625%;
         margin-left: -45.3125%;
     }
-    .modal-inner{
+    .modal.modal-in:not(.toast) .modal-inner{
         padding: 1.2rem;
     }
-    .modal-button{
+    .modal.modal-in:not(.toast) .modal-button{
         color: #3d4145;
     }
 </style>
@@ -17,7 +17,7 @@
     <div class="page-group" id="page-getCoupon">
         <div id="page-nav-bar" class="page page-current">
             <div class="content">
-            <a href="javascript:history.back(-1);" class="back deta-back" style="z-index: 4;"></a>
+                <a href="javascript:history.back(-1);" class="back deta-back" style="z-index: 4;"></a>
                 <div class="getCoupon">
                     <div class="bg-header"></div>
                     <div class="bg-footer"></div>
