24dce74dc0b58ba363fc30b2cab5132bc0c99efb
chendong <chendong@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2017-01-09 03:23:41 +0000

    红包口令页面

---
+++ tpl/lottery_red_web.php
@@ -0,0 +1,32 @@
+<?php include_once('header_web.php');?>
+<?php include_once('wxshare_web.php');?>
+<style>
+    .modal.modal-in:not(.toast){
+        width: 90.625%;
+        margin-left: -45.3125%;
+    }
+    .modal.modal-in:not(.toast) .modal-inner{
+        padding: 1.2rem;
+    }
+    .modal.modal-in:not(.toast) .modal-button{
+        color: #3d4145;
+    }
+</style>
+
+<body>
+    <div class="page-group" id="page-getCoupon">
+        <div id="page-nav-bar" class="page page-current">
+            <div class="content">
+            <a href="javascript:history.back(-1);" class="back deta-back" style="z-index: 4;"></a>
+                <div class="getCoupon">
+                    <div class="bg-header"></div>
+                    <div class="bg-footer"></div>
+                    <form class="form">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
