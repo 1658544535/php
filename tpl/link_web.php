0e5623d90b17ee376bcec0a82d7da339ef4f6659
zhengshaowei <zhengshaowei@804ee1a8-b229-4ccc-8a08-c3058f68b53b>
2017-01-24 09:12:22 +0000

    调整外链页面

--- tpl/link_web.php
+++ tpl/link_web.php
@@ -2,10 +2,10 @@
 <script type="text/javascript" src="/js/jweixin-1.0.0.js"></script>
 <script type="text/javascript" src="/js/wxshare.js"></script>
 <script type="text/javascript">
-var imgUrl = "<?php echo $site;?>images/gonggao200.jpg";
+var imgUrl = "<?php echo $site;?>images/hongbao.jpg";
 var link = "<?php echo $site;?>link.php?url=<?php echo $url;?>&title=<?php echo $title;?>";
-var title ="【拼得好】春节停止发货公告";
-var desc = "1月16日起，春节放价不放假，照常接单暂缓发货！";
+var title = '【拼得好】恭贺新春！';
+var desc = '春节不打烊，福利不停歇！';
 wxshare(false, '<?php echo $wxShareParam['appId'];?>', <?php echo $wxShareParam['timestamp'];?>, '<?php echo $wxShareParam['nonceStr'];?>', '<?php echo $wxShareParam['signature'];?>', imgUrl, link, title, desc);
 </script>
 <body>
